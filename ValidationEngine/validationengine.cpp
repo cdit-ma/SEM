737d7d55e64727b0de46bf79dd56642ac12361cb
Dan Fraser <danielrfraser@users.noreply.github.com>
2014-11-13 05:38:06 +0000

    aRAsd

---
+++ ValidationEngine/validationengine.cpp
@@ -0,0 +1,28 @@
+#include "validationengine.h"
+#include <QDebug>
+ValidationEngine::ValidationEngine()
+{
+
+}
+
+
+void ValidationEngine::addPlugin(ValidationPlugin *plugin)
+{
+    if(plugin && !plugins.contains(plugin)){
+        qDebug()<< "Added Plugin: " << plugin->getName();
+        plugins.append(plugin);
+    }
+}
+
+bool ValidationEngine::validateModel(Model *model)
+{
+    foreach(ValidationPlugin* vP, plugins){
+        qDebug() << "Validating Model against " << vP->getName();
+        bool result = vP->validate(model);
+        if(!result){
+            qCritical() << "Validation failed!";
+            return false;
+        }
+    }
+    return true;
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QtDebug>
 
 #include "medeawindow.h"
+#include "mainwindow.h"
 #include <QDebug>
 
 
@@ -9,6 +10,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MedeaWindow w;
+    MainWindow w2;
+
     w.show();
+    w2.show();
+
     return a.exec();
 }
