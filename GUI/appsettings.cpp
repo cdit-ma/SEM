caf77837443c4b3c59a8a240a904ac6781e93296
Dan Fraser <danielrfraser@users.noreply.github.com>
2015-04-10 09:26:35 +0000

    TEST

---
+++ GUI/appsettings.cpp
@@ -0,0 +1,81 @@
+#include "appsettings.h"
+#include <QWidget>
+#include <QObject>
+#include <QSettings>
+#include <QString>
+#include <QDebug>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QGroupBox>
+#include <QLineEdit>
+
+
+AppSettings::AppSettings(QWidget *parent):QObject(parent)
+{
+    settings = new QSettings("settings.ini", QSettings::IniFormat);
+
+}
+
+QSettings* AppSettings::getSettings()
+{
+    return settings;
+}
+
+void AppSettings::settingChanged(QString g , QString n , QString v)
+{
+    settings->beginGroup(g);
+     settings->setValue(n,v);
+     settings->endGroup();
+}
+
+
+void AppSettings::launchSettingsUI()
+{
+    QWidget* settingsGui = new QWidget(0);
+
+
+
+
+    QVBoxLayout *settingsLayout = new QVBoxLayout();
+    settingsGui->setLayout(settingsLayout);
+
+    foreach(QString group, settings->childGroups()){
+
+
+
+        //settingsLayout->addLayout(groupLayout);
+        //QLabel* groupSetting = new QLabel(group);
+        //groupLayout->addWidget(groupSetting);
+
+        settings->beginGroup(group);
+
+
+        QGroupBox* groupBox = new QGroupBox();
+        groupBox->setTitle(group);
+        settingsLayout->addWidget(groupBox);
+
+        QVBoxLayout* groupLayout = new QVBoxLayout();
+        groupBox->setLayout(groupLayout);
+
+        foreach(QString key, settings->childKeys()){
+            qCritical() << settings->value(key).typeName();
+            KeyEditWidget* keyEdit = new KeyEditWidget(0,group, key, settings->value(key));
+            connect(keyEdit, SIGNAL(valueChanged(QString,QString,QString)), this, SLOT(settingChanged(QString,QString,QString)));
+            groupLayout->addLayout(keyEdit->getLayout());
+        }
+        settings->endGroup();
+    }
+
+
+    //Read settings.
+
+
+
+    settingsGui->show();
+
+}
+
+void AppSettings::updateSetting()
+{
+
+}
---
+++ GUI/appsettings.h
@@ -0,0 +1,35 @@
+
+#ifndef APPSETTINGS_H
+#define APPSETTINGS_H
+
+#include <QSettings>
+#include <QObject>
+#include "keyeditwidget.h"
+
+class AppSettings: public QObject
+{
+    Q_OBJECT
+public:
+    AppSettings(QWidget* parent);
+    QSettings* getSettings();
+
+
+
+public slots:
+    void settingChanged(QString, QString, QString);
+    void launchSettingsUI();
+    void updateSetting();
+
+private:
+    QSettings* settings;
+
+
+
+
+
+
+
+};
+
+
+#endif
