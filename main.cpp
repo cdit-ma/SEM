c4a393d769099aa0c3a661c565c5503bf10931e8
danielrfraser <daniel.raymond.fraser@gmail.com>
2016-11-29 10:34:14 +1030

    Working Example

--- main.cpp
+++ main.cpp
@@ -13,13 +13,18 @@ int main(int argc, char** argv){
     SenderImpl* sender_impl = new SenderImpl();
     RecieverImpl* reciever_impl = new RecieverImpl();
     
+    //Construct Ports
     rxMessageInt* rxMessage = new test_rxMessage(reciever_impl);
     txMessageInt* txMessage = new test_txMessage(sender_impl, rxMessage);
 
+    //Attach Ports
     sender_impl->txMessage_ = txMessage;
+    reciever_impl->rxMessage_ = rxMessage;
+
+
 
     int i = 60;
-    while(--i > 0){
+    while(i-- > 0){
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         sender_impl->periodic_event();
     }
