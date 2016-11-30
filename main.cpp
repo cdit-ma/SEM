5eed33fb314b9a43a69432440a87cde9ab062dee
mitchell-conrad <mitchell.w.conrad@gmail.com>
2016-11-30 16:32:11 +1030

    DDS compiles.

--- main.cpp
+++ main.cpp
@@ -10,13 +10,24 @@
 #include "test/testrxmessage.h"
 #include "zmq/zmqrxmessage.h"
 #include "zmq/zmqtxmessage.h"
+#include "ndds/ndds_cpp.h"
 
 #include "zmq.hpp"
 
+void setupDDS(){
+	//DDSDomainParticipantFactory* factory = DDSDomainParticipantFactory::get_instance()
 
+    DDSDomainParticipant *participant = DDSDomainParticipantFactory::get_instance()->
+                        create_participant(
+                        9,
+                        DDS_PARTICIPANT_QOS_DEFAULT,
+                        NULL,   /* Listener */
+                        DDS_STATUS_MASK_NONE);
+
+}
 
 int main(int argc, char** argv){
-    SenderImpl* sender_impl = new SenderImpl();
+    //SenderImpl* sender_impl = new SenderImpl();
     RecieverImpl* reciever_impl = new RecieverImpl();
     
     //Construct Ports
@@ -24,22 +35,24 @@ int main(int argc, char** argv){
     //txMessageInt* txMessage = new test_txMessage(sender_impl, rxMessage);
 
     zmq::context_t * context = new zmq::context_t(1);
-    txMessageInt* txMessage = new zmq_txMessage(sender_impl, context, std::string("tcp://*:6000"));
+    //txMessageInt* txMessage = new zmq_txMessage(sender_impl, context, std::string("tcp://*:6000"));
+
+
 
     rxMessageInt* rxMessage = new zmq_rxMessage(reciever_impl, context, std::string("tcp://192.168.111.187:6000"));
     
     //Attach Ports
-    sender_impl->txMessage_ = txMessage;
+    //sender_impl->txMessage_ = txMessage;
     reciever_impl->rxMessage_ = rxMessage;
 
-    sender_impl->set_instName("SenderImpl");
-    sender_impl->set_message("Hello, World!");
+    //sender_impl->set_instName("SenderImpl");
+    //sender_impl->set_message("Hello, World!");
 
 
     int i = 60;
     while(i-- > 0){
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        sender_impl->periodic_event();
+        //sender_impl->periodic_event();
     }
 
     return -1;
