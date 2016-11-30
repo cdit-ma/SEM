daad49bfa3e8c00a4b449df854b6a5820842b43e
mitchell-conrad <mitchell.w.conrad@gmail.com>
2016-11-30 15:43:41 +1030

    DDS now working.

---
+++ dds/convert.h
@@ -0,0 +1,13 @@
+#ifndef DDS_CONVERT_H
+#define DDS_CONVERT_H
+
+#include "message.h"
+#include "messageSupport.h"
+
+#include "../message.h"
+
+
+    test_dds::Message* message_to_dds(::Message* message);
+    ::Message* dds_to_message(test_dds::Message* message);
+
+#endif // DDS_CONVERT_H
