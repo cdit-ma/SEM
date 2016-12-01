55b8931d9975ea3dca1fe15c189a6b183fa875c7
danielrfraser <daniel.raymond.fraser@gmail.com>
2016-12-01 12:00:38 +1030

    Playing with Mac and dds

--- main.cpp
+++ main.cpp
@@ -10,32 +10,21 @@
 #include "test/testrxmessage.h"
 #include "zmq/zmqrxmessage.h"
 #include "zmq/zmqtxmessage.h"
-#include "dds/ddstxmessage.h"
-#include "dds/ddsrxmessage.h"
-#include "ndds/ndds_cpp.h"
-#include "dds/messageSupport.h"
+//#include "dds/ddstxmessage.h"
+//#include "dds/ddsrxmessage.h"
+//#include "ndds/ndds_cpp.h"
+//#include "dds/messageSupport.h"
 
 #include "zmq.hpp"
 
-void setupDDS(){
-	//DDSDomainParticipantFactory* factory = DDSDomainParticipantFactory::get_instance()
-
-    
-
-
-
-
-
-}
-
 int main(int argc, char** argv){
-
+/*
 
     DDSDomainParticipant *participant = DDSDomainParticipantFactory::get_instance()->
                         create_participant(
                         9,
                         DDS_PARTICIPANT_QOS_DEFAULT,
-                        NULL,   /* Listener */
+                        NULL,
                         DDS_STATUS_MASK_NONE);
 
     DDS_PublisherQos pub_qos;
@@ -60,13 +49,13 @@ test_dds::MessageTypeSupport::register_type(participant, type_name);
             DDSTopic* topic = participant->create_topic("TOPIC", type_name,
                                       topic_qos, NULL,
                                       DDS_STATUS_MASK_ALL);
-
+*/
     SenderImpl* sender_impl = new SenderImpl();
-    RecieverImpl* reciever_impl = new RecieverImpl();
+    //RecieverImpl* reciever_impl = new RecieverImpl();
     
     //Construct Ports
-    rxMessageInt* rxMessage = new dds_rxMessage(reciever_impl, subscriber, topic);
-    txMessageInt* txMessage = new dds_txMessage(sender_impl, publisher, topic);
+    //rxMessageInt* rxMessage = new dds_rxMessage(reciever_impl, subscriber, topic);
+  //  txMessageInt* txMessage = new dds_txMessage(sender_impl, publisher, topic);
 
     //zmq::context_t * context = new zmq::context_t(1);
     //txMessageInt* txMessage = new zmq_txMessage(sender_impl, context, std::string("tcp://*:6000"));
@@ -74,8 +63,8 @@ test_dds::MessageTypeSupport::register_type(participant, type_name);
     //rxMessageInt* rxMessage = new zmq_rxMessage(reciever_impl, context, std::string("tcp://192.168.111.187:6000"));
     
     //Attach Ports
-    sender_impl->txMessage_ = txMessage;
-    reciever_impl->rxMessage_ = rxMessage;
+  //  sender_impl->txMessage_ = txMessage;
+    //reciever_impl->rxMessage_ = rxMessage;
 
     sender_impl->set_instName("SenderImpl");
     sender_impl->set_message("Hello, World!");
