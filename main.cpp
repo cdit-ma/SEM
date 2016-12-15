011b6fc6823ab37e25626b4f8be98ab9faa627d6
mitchell-conrad <mitchell.w.conrad@gmail.com>
2016-12-15 14:52:57 +1030

    Fix # in topic name.

--- main.cpp
+++ main.cpp
@@ -27,22 +27,33 @@
 int main(int argc, char** argv){
     
     SenderImpl* sender_impl = new SenderImpl();
-    ReceiverImpl* receiver_impl = new ReceiverImpl();
-
     SenderImpl* sender_impl2 = new SenderImpl();
+    SenderImpl* sender_impl3 = new SenderImpl();
+    SenderImpl* sender_impl4 = new SenderImpl();
+
+    ReceiverImpl* receiver_impl = new ReceiverImpl();
     ReceiverImpl* receiver_impl2 = new ReceiverImpl();
+    ReceiverImpl* receiver_impl3 = new ReceiverImpl();
+    ReceiverImpl* receiver_impl4 = new ReceiverImpl();
+
 
     sender_impl->set_instName("RTI_SENDER");
     sender_impl2->set_instName("OSPL_SENDER");
+    sender_impl3->set_instName("ZMQ_SENDER");
+    sender_impl4->set_instName("QPID_SENDER");
     
     sender_impl->set_message("YO");
     sender_impl2->set_message("YO");
+    sender_impl3->set_message("YO");
+    sender_impl4->set_message("YO");
     
     receiver_impl->set_instName("RTI_receiveR");
     receiver_impl2->set_instName("OSPL_receiveR");
+    receiver_impl3->set_instName("zmq_receiveR");
+    receiver_impl4->set_instName("qpid_receiveR");
     
-    std::string topic_name("Topic#1");
-    std::string topic_name2("Topic#2");
+    std::string topic_name("Topic1");
+    std::string topic_name2("Topic2");
 
     std::string pub_name("pub");
     std::string sub_name("sub");
@@ -59,37 +70,42 @@ int main(int argc, char** argv){
     rxMessageInt* zmq_rx = 0;
 
     //RTI DDS
-    //rti_tx = new rti::TxMessage(sender_impl, 0, pub_name, topic_name);
-    //rti_rx = new rti::RxMessage(receiver_impl, 0, sub_name, topic_name);
+    rti_tx = new rti::TxMessage(sender_impl, 0, pub_name, topic_name);
+    rti_rx = new rti::RxMessage(receiver_impl, 0, sub_name, topic_name);
 
     //OpenSplice DDS
     ospl_tx = new ospl::TxMessage(sender_impl2, 0, pub_name, topic_name);
     ospl_rx = new ospl::RxMessage(receiver_impl2, 0, sub_name, topic_name);
 
-    //QPID
-    //qpid_tx = new qpid::TxMessage(sender_impl, "localhost:5672", "a");
-    //qpid_rx = new qpid::RxMessage(receiver_impl, "localhost:5672",  "a");
-    
     //ZMQ
-    zmq_tx = new zmq::TxMessage(sender_impl, std::string("tcp://*:6000"));
-    zmq_rx = new zmq::RxMessage(receiver_impl, std::string("tcp://localhost:6000"));
+    zmq_tx = new zmq::TxMessage(sender_impl3, std::string("tcp://*:6000"));
+    zmq_rx = new zmq::RxMessage(receiver_impl3, std::string("tcp://localhost:6000"));
+
+    //QPID
+    qpid_tx = new qpid::TxMessage(sender_impl4, "localhost:5672", "a");
+    qpid_rx = new qpid::RxMessage(receiver_impl4, "localhost:5672",  "a");
+    
     
     //Attach Ports
     sender_impl->_set_txMessage(rti_tx);
-    receiver_impl->_set_rxMessage(rti_rx);
-
     sender_impl2->_set_txMessage(ospl_tx);
+    sender_impl3->_set_txMessage(zmq_tx);
+    sender_impl4->_set_txMessage(qpid_tx);
+
+    receiver_impl->_set_rxMessage(rti_rx);
     receiver_impl2->_set_rxMessage(ospl_rx);
+    receiver_impl3->_set_rxMessage(zmq_rx);
+    receiver_impl4->_set_rxMessage(qpid_rx);
 
 
     int i = 600;
     while(i-- > 0){
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-        sender_impl->set_message(std::to_string(i));
         sender_impl->periodic_event();
-        sender_impl2->set_message(std::to_string(i));
         sender_impl2->periodic_event();
+        sender_impl3->periodic_event();
+        sender_impl4->periodic_event();
     }
 
     return -1;
