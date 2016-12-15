af0405c3a99c58e47d52de76c415aea198e1dfa3
mitchell-conrad <mitchell.w.conrad@gmail.com>
2016-12-15 15:46:28 +1030

    Done

--- runtime/receiverimpl.cpp
+++ runtime/receiverimpl.cpp
@@ -1,7 +1,6 @@
 #include "receiverimpl.h"
 #include <iostream>
 
-
 void ReceiverImpl::rxMessage(Message* message){
-    std::cout << instName() << "RX Message: "  << message->content() << " From: " << message->instName() << std::endl; 
+    std::cout << instName() << " Got Message: "  << message->content() << " From: " << message->instName() << std::endl; 
 }
