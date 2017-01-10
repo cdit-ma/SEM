f43b814c824fef20a24b0347fdbcda4cf19379ac
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-01-10 15:43:16 +1030

    Remove runtime prototyping.

--- runtime/receiverimpl.cpp
+++
@@ -1,6 +0,0 @@
-#include "receiverimpl.h"
-#include <iostream>
-
-void ReceiverImpl::rxMessage(Message* message){
-    std::cout << instName() << " Got Message: "  << message->content() << " From: " << message->instName() << std::endl; 
-}
