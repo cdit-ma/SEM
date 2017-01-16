10f26c2333b454bcc2733a73b6aead0141a08813
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-01-16 17:07:28 +1030

    Removed print lines from periodic event

--- common/includes/core/periodiceventport.cpp
+++ common/includes/core/periodiceventport.cpp
@@ -45,7 +45,6 @@ bool PeriodicEventPort::wait_for_tick(){
 
 void PeriodicEventPort::loop(){
     while(true){
-        std::cout << "TICK!" << std::endl;
         if(callback_ != nullptr){
             callback_();
         }
