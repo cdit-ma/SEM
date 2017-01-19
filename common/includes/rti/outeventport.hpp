46745ece6307485206a0dede28306aadc3a4ed2d
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-01-19 13:56:57 +1030

    Implement rti activate/passivate.

--- common/includes/rti/outeventport.hpp
+++ common/includes/rti/outeventport.hpp
@@ -10,9 +10,24 @@
 namespace rti{
      template <class T, class S> class OutEventPort: public ::OutEventPort<T>{
         public:
-            OutEventPort(Component* component, int domain_id, std::string publisher_name, std::string topic_name);
+            OutEventPort(Component* component, std::string name);
             void tx(T* message);
+
+            void startup(std::map<std::string, ::Attribute*> attributes);
+            void teardown();
+
+            bool activate();
+            bool passivate();
         private:
+            std::mutex control_mutex_;
+
+            bool configured_ = false;
+
+            std::string topic_name_;
+            int domain_id_;
+            std::string publisher_name_;
+
+
             dds::pub::DataWriter<S> writer_ = dds::pub::DataWriter<S>(dds::core::null);
     }; 
 };
@@ -30,14 +45,58 @@ void rti::OutEventPort<T, S>::tx(T* message){
 };
 
 template <class T, class S>
-rti::OutEventPort<T, S>::OutEventPort(Component* component, int domain_id, std::string publisher_name, std::string topic_name):
-::OutEventPort<T>(component){
+rti::OutEventPort<T, S>::OutEventPort(Component* component, std::string name):
+::OutEventPort<T>(component, name){};
+
+template <class T, class S>
+void rti::OutEventPort<T, S>::startup(std::map<std::string, ::Attribute*> attributes){
+    std::lock_guard<std::mutex> lock(control_mutex_);
+
+    if(attributes.count("publisher_name")){
+        publisher_name_ = attributes["publisher_name"]->s;
+        configured_ = true;
+    }
+    if(attributes.count("topic_name")){
+        topic_name_ = attributes["topic_name"]->s;
+        configured_ = true && configured_;
+    }
+    if(attributes.count("domain_id")){
+        domain_id_ = attributes["domain_id"]->s;
+        configured_ = true && configured_;                
+    }
+};
+
+template <class T, class S>
+void rti::OutEventPort<T, S>::teardown(){
+    passivate();
+    std::lock_guard<std::mutex> lock(control_mutex_);
+    configured_ = false;
+};
+
+template <class T, class S>
+bool rti::OutEventPort<T, S>::activate(){
+    std::lock_guard<std::mutex> lock(control_mutex_);
+
+
     //Construct a DDS Participant, Publisher, Topic and Writer
     auto helper = DdsHelper::get_dds_helper();   
-    auto participant = helper->get_participant(domain_id);
-    auto publisher = helper->get_publisher(participant, publisher_name);
-    auto topic = helper->get_topic<S>(participant, topic_name);
+    auto participant = helper->get_participant(domain_id_);
+    auto publisher = helper->get_publisher(participant, publisher_name_);
+    auto topic = helper->get_topic<S>(participant, topic_name_);
     writer_ = helper->get_data_writer<S>(publisher, topic);
+
+    return ::OutEventPort<T>::activate();
+};
+
+template <class T, class S>
+bool rti::OutEventPort<T, S>::passivate(){
+    std::lock_guard<std::mutex> lock(control_mutex_);
+
+    if(writer_){
+        delete this->writer_;
+    }
+
+    return ::OutEventPort<T>passivate();
 };
 
 #endif //RTI_OUTEVENTPORT_H
