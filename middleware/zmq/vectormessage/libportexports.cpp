4ebbb4f872e00c9106fab80b9e96014e4ac7cf8c
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-02-06 13:09:59 +1030

    Still missing files

---
+++ middleware/zmq/vectormessage/libportexports.cpp
@@ -0,0 +1,23 @@
+#include <core/libportexports.h>
+
+#include "../../proto/vectormessage/convert.h"
+
+#include <zmq/ineventport.hpp>
+#include <zmq/outeventport.hpp>
+
+EventPort* construct_rx(std::string port_name, Component* component){
+    EventPort* p = 0;
+    if(component){
+        //Get the callback function
+        auto fn = component->get_callback(port_name);    
+        if(fn){
+            p = new zmq::InEventPort<::VectorMessage, cdit::VectorMessage>(component, port_name, fn);
+        }
+    }
+    return p;
+};
+
+
+EventPort* construct_tx(std::string port_name, Component* component){
+    return new zmq::OutEventPort<::VectorMessage, cdit::VectorMessage>(component, port_name);
+};
