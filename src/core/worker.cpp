3cff99f958a6cd80073b17b87db2dafa24b67a04
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-03-06 15:18:44 +1030

    Added worker

---
+++ src/core/worker.cpp
@@ -0,0 +1,38 @@
+#include "worker.h"
+#include <stdio.h>
+#include <iostream>
+
+Worker::Worker(Component* component, std::string worker_name, std::string inst_name){
+    set_name(inst_name);
+    component_ = component;
+    worker_name_ = worker_name;
+};
+
+Worker::~Worker(){
+
+};
+
+std::string Worker::get_worker_name(){
+    return worker_name_;
+};
+
+int Worker::get_new_work_id(){
+    std::unique_lock<std::mutex>(mutex_);
+    return work_id_ ++;
+};
+
+Component* Worker::get_component(){
+    return component_;
+};
+
+std::string Worker::get_arg_string(std::string str_format, ...){
+    va_list args;
+    va_start (args, str_format);
+
+    auto fmt = str_format.c_str();
+    int len = snprintf(NULL, 0, fmt, args) + 1;
+    char buffer[len];
+    vsprintf(buffer, fmt, args);
+    va_end (args);
+    return std::string(buffer);
+};
---
+++ src/core/worker.h
@@ -0,0 +1,28 @@
+#ifndef CORE_WORKER_H
+#define CORE_WORKER_H
+
+#include <mutex>
+#include <string>
+
+#include "component.h"
+#include "activatable.h"
+
+class Worker: public Activatable{
+    public:
+        Worker(Component* component, std::string worker_name, std::string inst_name);
+        virtual ~Worker();
+
+        Component* get_component();
+        std::string get_worker_name();
+    protected:
+        int get_new_work_id();
+        
+        std::string get_arg_string(std::string str_format, ...);
+    private:
+        Component* component_ = 0;
+        std::mutex mutex_;
+        std::string worker_name_;
+        int work_id_ = 0;
+};
+
+#endif //CORE_WORKER_H
