1eb7f14d8949f2379cb17506c18e0fd5a1500c7f
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-04-26 12:08:34 +0930

    Add mwip profiler. RUN-56

---
+++ src/workers/cpu/cpu_worker_profiler.cpp
@@ -0,0 +1,59 @@
+#include "cpu_worker.h"
+#include <chrono>
+#include <iostream>
+
+#define RUN_COUNT 1000
+
+std::string dblquotewrap(std::string val){
+    std::string quote("\"");
+    return quote + val + quote;
+}
+std::string tab(int i = 1){
+    return std::string(i, '\t');
+}
+
+std::string json_pair(std::string key, std::string val){
+    return dblquotewrap(key) + ": " + dblquotewrap(val);
+}
+
+std::string json_bool_pair(std::string key, bool val){
+    return dblquotewrap(key) + ": " + (val ? "true" : "false");
+}
+
+std::string json_export_list(std::vector<std::string> vals){
+    std::string str;
+    for(auto val : vals){
+        bool is_last = val == vals.back();
+        std::string comma = (is_last ? "" : ",\n");
+        str += val + comma;
+    }
+    return str;
+}
+
+std::string json_list_pair(std::string key, std::vector<std::string> vals){
+    std::string str = dblquotewrap(key) + ": [";
+    for(auto val : vals){
+        bool is_last = val == vals.back();
+        std::string comma = (is_last ? "" : ", ");
+        str += dblquotewrap(val) + comma;
+    }
+    str+= "]";
+    return str;
+}
+
+int main(){
+    Cpu_Worker worker(0, "cpu_worker");
+
+    auto start = std::chrono::steady_clock::now();
+    
+    int result = worker.MWIP(RUN_COUNT);
+    auto end = std::chrono::steady_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::string newline("\n");
+
+    std::string output = "{" + newline + tab() + json_pair("MWIP_microseconds", std::to_string(ms.count()/RUN_COUNT)) + newline + "}";
+
+    std::cout << output << std::endl;
+
+}
