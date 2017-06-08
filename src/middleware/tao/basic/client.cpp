a400943209d52552d557ee1a3a2c735574344df8
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-06-08 16:11:41 +0930

    adding cmake

--- src/middleware/tao/basic/client.cpp
+++ src/middleware/tao/basic/client.cpp
@@ -3,6 +3,16 @@
 
 int main(int argc, char** argv){
     auto orb = CORBA::ORB_init (argc, argv);
+
+    // Convert the string to an object.
+    ::CORBA::Object_var obj = orb->string_to_object (client);
+
+    if (::CORBA::is_nil (obj.in ()))
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("%T (%t) - %M - failed to convert string to object\n")),
+                         -1);
+
+                         /*
     std::cout << "Resolving LoggingServer" << std::endl;
     ::CORBA::Object_var obj = orb->resolve_initial_references ("LoggingServer");
     std::cout << "Resolved LoggingServer" << std::endl;
@@ -11,6 +21,14 @@ int main(int argc, char** argv){
                        ACE_TEXT ("%T (%t) - %M - failed to resolve LoggingServer\n")),
                        -1);
 
+    // Convert the string to an object.
+    ::CORBA::Object_var obj = this->orb_->string_to_object (client);
+
+    if (::CORBA::is_nil (obj.in ()))
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("%T (%t) - %M - failed to convert string to object\n")),
+                         -1);
+*/
     std::cout << "Connecting to server" << std::endl;
     auto sender = Test::Hello::_narrow(obj.in());
 
