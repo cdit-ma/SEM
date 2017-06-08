f5da013cbb215414a89fd56380bd10cfc1a91392
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-06-08 15:39:12 +0930

    adding cmake

--- src/middleware/tao/basic/server.cpp
+++ src/middleware/tao/basic/server.cpp
@@ -18,51 +18,71 @@ int main(int argc, char ** argv){
     // Initialze the ORB.
     CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
 
-    ::CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
-    auto root_poa_ = ::PortableServer::POA::_narrow (obj.in ());
+    // Get a reference to the RootPOA.
+    CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
 
-    // Activate the RootPOA's manager.
-    ::PortableServer::POAManager_var mgr = root_poa_->the_POAManager ();
-    mgr->activate ();
+    // Get the POA_var object from Object_var.
+    PortableServer::POA_var root_poa =
+      PortableServer::POA::_narrow (obj.in ());
 
-    // Construct the policy list for the LoggingServerPOA.
-    CORBA::PolicyList policies (6);
-    policies.length (6);
+    // Get the POAManager of the RootPOA.
+    PortableServer::POAManager_var poa_manager =
+      root_poa->the_POAManager ();
 
-    policies[0] = root_poa_->create_thread_policy (PortableServer::ORB_CTRL_MODEL);
-    policies[1] = root_poa_->create_servant_retention_policy (PortableServer::RETAIN);
-    policies[2] = root_poa_->create_id_assignment_policy (PortableServer::SYSTEM_ID);
-    policies[3] = root_poa_->create_id_uniqueness_policy (PortableServer::UNIQUE_ID);
-    policies[4] = root_poa_->create_lifespan_policy (PortableServer::TRANSIENT);
-    policies[5] = root_poa_->create_request_processing_policy (PortableServer::USE_ACTIVE_OBJECT_MAP_ONLY);
+    poa_manager->activate ();
 
-    // Create the child POA for the test logger factory servants.
-    ::PortableServer::POA_var child_poa =
-    root_poa_->create_POA ("LoggingServerPOA",
-                                   ::PortableServer::POAManager::_nil (),
-                                   policies);
+    // Create a USER_ID IdAssignmentpolicy object.
+    PortableServer::IdAssignmentPolicy_var idassignment =
+      root_poa->create_id_assignment_policy (PortableServer::USER_ID);
 
-    // Destroy the POA policies
-    for (::CORBA::ULong i = 0; i < policies.length (); ++ i)
-      policies[i]->destroy ();
+    // Create a PERSISTENT LifespanPolicy object.
+    PortableServer::LifespanPolicy_var lifespan =
+      root_poa->create_lifespan_policy (PortableServer::PERSISTENT);
 
-    mgr = child_poa->the_POAManager ();
-    mgr->activate ();
+    // Policies for the childPOA to be created.
+    CORBA::PolicyList policies;
+    policies.length (2);
 
-    // Activate the servant.
-    ACE_DEBUG ((LM_DEBUG,
-                ACE_TEXT ("%T (%t) - %M - activating logger server servant\n")));
+    policies[0] =
+      PortableServer::IdAssignmentPolicy::_duplicate (idassignment.in ());
 
+    policies[1] =
+      PortableServer::LifespanPolicy::_duplicate (lifespan.in ());
+
+    // Create the childPOA under the RootPOA.
+    PortableServer::POA_var child_poa =
+      root_poa->create_POA ("asd",
+                            poa_manager.in (),
+                            policies);
+
+    // Destroy policy objects.
+    idassignment->destroy ();
+    lifespan->destroy ();
+
+        
     Hello *hello_impl = 0;
-    ACE_NEW_RETURN (hello_impl, Hello (child_poa.in ()), 1);
+    ACE_NEW_RETURN (hello_impl, Hello (orb.in ()), 1);
+    
 
-    orb->run ();
-
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) server - event loop finished\n"));
-    root_poa_->destroy (1, 1);
-    //child_poa->destroy ();
-
-    return 0;
+    // Get the Object ID.
+    PortableServer::ObjectId_var oid =
+      PortableServer::string_to_ObjectId ("asd");
 
 
+    // Activate the Stock_Factory object.
+    child_poa->activate_object_with_id (oid.in (), hello_impl);
+
+    // Get the object reference.
+    CORBA::Object_var stock_factory = child_poa->id_to_reference (oid.in ());
+
+    // Stringify all the object referencs.
+    CORBA::String_var ior = orb->object_to_string (stock_factory.in ());
+    // Print them out !
+    std::cout << ior.in () << std::endl;
+
+    orb-> run ();
+
+    // Destroy POA, waiting until the destruction terminates.
+    root_poa->destroy (1, 1);
+    orb->destroy ();
 }
