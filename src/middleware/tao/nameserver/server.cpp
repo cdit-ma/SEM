b3c2775845408fa6d11557c8e3dad7667d336481
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-06-08 12:17:09 +0930

    adding cmake

--- src/middleware/tao/nameserver/server.cpp
+++ src/middleware/tao/nameserver/server.cpp
@@ -51,7 +51,7 @@ int main(int argc, char ** argv){
 
     // Create the childPOA under the RootPOA.
     PortableServer::POA_var child_poa =
-      root_poa->create_POA ("childPOA",
+      root_poa->create_POA ("Test",
                             poa_manager.in (),
                             policies);
 
@@ -66,17 +66,17 @@ int main(int argc, char ** argv){
 
     // Get the Object ID.
     PortableServer::ObjectId_var oid =
-      PortableServer::string_to_ObjectId ("Stock_Factory");
+      PortableServer::string_to_ObjectId ("HelloSender");
+
+      std::cout << oid.in() << std::endl;
+      std::cout << orb->object_to_string(oid.in()) << std::endl;
 
     // Activate the Stock_Factory object.
     child_poa->activate_object_with_id (oid.in (),
                                         hello_impl);
 
-
-
     // Get the object reference.
-    CORBA::Object_var stock_factory =
-    child_poa->id_to_reference (oid.in ());
+    CORBA::Object_var stock_factory = child_poa->id_to_reference (oid.in ());
 
     // Stringify all the object referencs.
     CORBA::String_var ior = orb->object_to_string (stock_factory.in ());
