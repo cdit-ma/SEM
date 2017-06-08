38ff8516375f2386450bf0598aba11e4686efea2
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-06-08 13:00:01 +0930

    adding cmake

--- src/middleware/tao/nameserver/client.cpp
+++ src/middleware/tao/nameserver/client.cpp
@@ -4,7 +4,7 @@
 #include "messageS.h"
 
 
-const char* get_ior(std::string ip, int port, std::string poa_name, std::string obj_name){
+std::string get_ior(std::string ip, int port, std::string poa_name, std::string obj_name){
     //corbaloc:iiop:192.168.111.90:12345/%14%01%0f%00NUP%00%00%00%1b%00%00%00%00%01%00%00%00RootPOA%00WHATISTHIS%00%00%00%00%00%01%00%00%00HelloSender
     std::stringstream ior;
     ior << "corbaloc:iiop:";
@@ -17,16 +17,16 @@ const char* get_ior(std::string ip, int port, std::string poa_name, std::string 
     ior << poa_name;
     ior << "%00%00%00%00%00%01%00%00%00";
     ior << obj_name;
-    return ior.str().c_str();
+    return ior.str();
 }
 
 int main(int argc, char** argv){
     auto orb = CORBA::ORB_init (argc, argv);
     //CORBA::Object_var ior = orb->string_to_object("corbaloc:iiop:1.2@192.168.111.90:12345/%14%01%0f%00NUP%00%00%00%19%00%00%00%00%01%00%00%00RootPOA%00childPOA%00%00%00%00%00%01%00%00%00Stock_Factory");
 
-    auto ior_c = get_ior("192.168.111.90", 12345, "Test", "HelloSender");
-    std::cout << ior_c << std::endl;
-    CORBA::Object_var ior = orb->string_to_object(ior_c);
+    auto ior_s = get_ior("192.168.111.90", 12345, "Test", "HelloSender");
+    std::cout << ior_s << std::endl;
+    CORBA::Object_var ior = orb->string_to_object(ior_s.c_str());
     auto sender = Test::Hello::_narrow(ior.in());
 
     Test::Hello_var sender_var = sender;
