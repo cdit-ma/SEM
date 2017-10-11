371c478d4b7b9bc90ec1a517192ccd0ce0e7c7a0
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-10-11 13:20:20 +1030

    updated

--- src/middleware/rti/ineventport.hpp
+++ src/middleware/rti/ineventport.hpp
@@ -137,7 +137,7 @@ void rti::InEventPort<T, S>::receive_loop(){
         auto topic = get_topic<S>(participant, topic_name_);
 
         auto subscriber = helper->get_subscriber(participant, subscriber_name_);
-        auto reader_ = get_data_reader<S>(subscriber, topic, qos_profile_path_, qos_profile_name_);
+        reader_ = get_data_reader<S>(subscriber, topic, qos_profile_path_, qos_profile_name_);
         
         //Construct a DDS Listener, designed to call back into the receive thread
         auto listener_ = new rti::DataReaderListener<T, S>(this);
