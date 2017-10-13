c3a0a1b76e2b353eb478125bd48ccb10d592dfbe
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-10-13 16:01:09 +1030

    Updated Git

--- src/Utils/IdlParser/main.cpp
+++ src/Utils/IdlParser/main.cpp
@@ -1,14 +1,6 @@
 #include "idlparser.h"
 #include <iostream>
 
-<<<<<<< HEAD
-int main(int argc, char** argv){
-    if(argc == 2){
-        std::cout << IdlParser::ParseIdl(argv[1], true) << std::endl;
-    }
-    return 0;
-};
-=======
 int main(int argc, char *argv[])
 {
     if(argc > 1){
@@ -19,4 +11,3 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
->>>>>>> develop
