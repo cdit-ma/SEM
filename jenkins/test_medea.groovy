1ddfc2a05a82b276f911e6c4ce748451dcb653c9
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-11-01 11:01:32 +1030

    Updated to use for loop MED-280

--- jenkins/test_medea.groovy
+++ jenkins/test_medea.groovy
@@ -2,7 +2,6 @@ def PROJECT_NAME = 'test_medea'
 
 //Run script, changes to bat if windows detected.
 def runScript(String script){
-    print(script)
     if(isUnix()){
         out = sh(returnStatus: true, script: script)
         return out
@@ -31,18 +30,37 @@ node("MEDEA"){
             }
         }
         
+        def test_count = 0
+        def test_error_count = 0
         stage("Test"){
             dir("test/bin"){
                 def globstr = "*"
+                if(!isUnix()){
+                    globstr = '*.exe'
+                }
+
+                //Find all executables
                 def test_list = findFiles glob: globstr
-
-
                 for (int i = 0; i < test_list.size(); i++){
+                    test_count ++
                     def file_path = test_list[i].name
                     print("Running Test: " + file_path)
-                    runScript("./" + file_path)
+                    test_error_count += runScript("./" + file_path)
                 }
             }
         }
+
+        currentBuild.description = 'Passed ' + (test_count - test_error_count)+ '/' + test_count + ' Test Cases'
+        if(test_count > 0){
+            if(test_error_count > 0){
+                currentBuild.result = 'Unstable'
+            }else{
+                currentBuild.result = 'Success'
+            }
+        }else{
+            currentBuild.result = 'Failure'
+        }
+
+        
     }
 }
