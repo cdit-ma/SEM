2cdba37b0d47d2de5558da7b89d83389513dcd6f
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-11-03 17:16:45 +1030

    Updating test

--- jenkins/update_re.groovy
+++ jenkins/update_re.groovy
@@ -1,10 +1,8 @@
 def git_path = "/srv/git/re";
 stage("checkout") {
     node("master"){
-        dir(output_dir){
-            sh 'rm -rf ' + git_path + '/*'
-            sh 'mv * ' + git_path
-        }
+        sh 'rm -rf ' + git_path + '/*'
+        sh 'mv * ' + git_path
         sh 'ls ' + git_path
         deleteDir();
     }
