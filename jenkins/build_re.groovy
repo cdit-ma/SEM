c9f6fe306d1b1e4dc3117df83c6d22c5e46ce2ba
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-11-06 13:30:06 +1030

    Update to use utils class. GEN-43

--- jenkins/build_re.groovy
+++ jenkins/build_re.groovy
@@ -17,7 +17,7 @@ stage("Checkout"){
 def step_build_test = [:]
 def step_test = [:]
 
-def re_nodes = getLabelledNodes("build_re")
+def re_nodes = utils.getLabelledNodes("build_re")
 for(n in re_nodes){
     def node_name = n
 
@@ -26,7 +26,7 @@ for(n in re_nodes){
             unstash "source_code"
             dir(PROJECT_NAME + "/build"){
                 //Build the entire project 
-                buildProject("Unix Makefiles", "-DBUILD_TEST=ON")
+                utils.buildProject("Unix Makefiles", "-DBUILD_TEST=ON")
             }
         }
     }
@@ -45,10 +45,10 @@ for(n in re_nodes){
                 dir("results"){
                     for(def file : test_list){
                         def file_path = file.name
-                        def file_name = trimExtension(file_path)
+                        def file_name = utils.trimExtension(file_path)
                         def test_output = file_name + "_" + node_name + ".xml"
                         print("Running Test: " + file_path)
-                        def test_error_code = runScript("../" + file_path + " --gtest_output=xml:" + test_output)
+                        def test_error_code = utils.runScript("../" + file_path + " --gtest_output=xml:" + test_output)
 
                         if(test_error_code != 0){
                             test_error_count ++
