2f04657c50e6e251970179e72437ce19a1bf4952
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-11-06 12:37:55 +1030

    Update to use jenkins shared library. GEN-43

--- jenkins/deploy_cmake_modules.groovy
+++ jenkins/deploy_cmake_modules.groovy
@@ -1,146 +1,38 @@
 /**
-This script will deploy cmake_modules to all jenkins slaves with the re label.
-This script requires the following Jenkins parameters:
-- "GIT_CREDENTIAL_ID" : Username with password credential
-- "GIT_URL" : String parameter (Defaulted to https://github.com/cdit-ma/cmake_modules)
-- "GIT_BRANCH" : Optional branch to checkout, overridden by GIT_TAG
-- "GIT_TAG" : Optional version tag, ignores any branch specified
+This script will deploy cmake_modules to all jenkins slaves with the cmake_modules label.
 This script requires the following Jenkins plugins:
 -Pipeline: Utility Steps
 */
 
+//Load shared pipeline utility library
+@Library('cditma-utils')
+import cditma.Utils
+
+def utils = new Utils(this);
+
 def PROJECT_NAME = 'cmake_modules'
+def git_url = "/srv/git"
+def cmake_modules_nodes = utils.getLabelledNodes("re");
 
-// This method collects a list of Node names from the current Jenkins instance
-@NonCPS
-def nodeNames() {
-  return jenkins.model.Jenkins.instance.nodes.collect { node -> node.name }
-}
-
-//Gets nodes label
-def getLabels(String name){
-    def computer = Jenkins.getInstance().getComputer(name)
-    def node = computer.getNode()
-    if(computer.isOnline()){
-        return node.getLabelString()
-    }
-    return ""
-}
-
-//Check all required params
-def checkParamsExist(){
-    def git_url = ""
-    git_url = env.GIT_URL
-    if(git_url == null){
-        print("ERROR: \"GIT_URL\" string parameter missing in build configuration")
-        return false
-    }
-    def git_credential_id = ""
-    git_credential_id = env.GIT_CREDENTIAL_ID
-    if(git_credential_id == null) {
-        print("ERROR: \"GIT_CREDENTIAL_ID\" credential parameter missing in build configuration")
-        return false
-    }
-    def branch = env.GIT_BRANCH
-    if(branch == null){
-        print("ERROR: \"GIT_BRANCH\" string parameter missing in build configuration")
-        return false
-    }
-
-    def tag = env.GIT_TAG
-    if(tag == null){
-        print("ERROR: \"GIT_TAG\" string parameter missing in build configuration")
-        return false
-    }
-
-    return true
-}
-
-def getCredentialID(){
-    return env.GIT_CREDENTIAL_ID
-}
-
-//Build git ref pointer. If no branch specified, use tag, if no tag specified use default branch (master).
-def buildGitRef(String branch, String tag){
-    def default_branch = "master"
-    def ref_name = ""
-
-    if(!branch){
-        branch = default_branch
-    }
-    ref_name = "*/" + branch
-
-    if(tag){
-        ref_name = "refs/tags/" + tag
-    }
-    return ref_name
-}
-
-//Build git url. If no url specified, default to cdit-ma github+proj name
-def buildGitUrl(String url, String proj){
-    def default_git_url = "https://github.com/cdit-ma/" + proj + ".git"
-    //Set git url to default if empty
-    if(!url){
-        return default_git_url
-    }
-    return url
-}
-
-
-def ref_name = ""
-def git_url = ""
-def git_credential_id = ""
-def filtered_names = []
-
-stage('Set up'){
-    //Start deploy script
-    if(!checkParamsExist()){
-        currentBuild.result = 'FAILURE'
-        return
-    }
-
-    //Get nodes to deploy to
-    def names = nodeNames()
-    for(n in names){
-        if(getLabels(n).contains("re")){
-            filtered_names << n
-            print("Got Node: " + n)
-        }
-    }
-
-    //Build git url and ref
-    git_credential_id = getCredentialID()
-    git_url = buildGitUrl(env.GIT_URL, PROJECT_NAME)
-    ref_name = buildGitRef(env.GIT_BRANCH, env.GIT_TAG)
-    currentBuild.description = git_url + '/' + ref_name
-}
-
-//Checkout logan on master node and stash
+//Checkout and stash cmake_modules source (stored on Master's local git repo)
 stage('Checkout'){
-    node("master"){
-        dir("cmake_modules"){
-            deleteDir()
-            checkout([$class: 'GitSCM', branches: [[name: ref_name]], doGenerateSubmoduleConfigurations: false,
-                extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: false, reference: '', trackingSubmodules: false]],
-                submoduleCfg: [], userRemoteConfigs: [[credentialsId: git_credential_id, url: git_url]]])
-
-            stash includes: "**", name: "cmake_modules_source"
+    node('master'){
+        dir(git_url + "/" + PROJECT_NAME){
+            stash include: "**", name: "source_code"
         }
     }
 }
 
-//Unstash on all logan nodes
-stage('Checkout'){
-    def builders = [:]
-    for(n in filtered_names){
-        def node_name = n
-        builders[node_name] = {
-            node(node_name){
-                dir(env.CMAKE_MODULE_PATH){
-                    unstash "cmake_modules_source"
-                }
+//Construct build map for all nodes which should build
+def step_build = [:]
+for(n in cmake_modules_nodes){
+    def node_name = n
+    step_build[node_name] = {
+        node(node_name){
+            dir("${CMAKE_MODULE_PATH}"){
+                deleteDir()
+                unstash "source_code"
             }
         }
     }
-    parallel builders
 }
