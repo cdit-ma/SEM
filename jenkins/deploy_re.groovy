b59e2e5b4714d02cbaea9f4d349fb876648fff71
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-11-06 11:56:38 +1030

    Updated deploy_re to use pipeline library GEN-40

--- jenkins/deploy_re.groovy
+++ jenkins/deploy_re.groovy
@@ -4,6 +4,7 @@ This script requires the following Jenkins plugins:
 -Pipeline: Utility Steps
 */
 
+//Load shared pipeline utility library
 @Library('cditma-utils')
 import cditma.Utils
 
@@ -13,7 +14,7 @@ def PROJECT_NAME = 're'
 def git_url = "/srv/git"
 def re_nodes = utils.getLabelledNodes(PROJECT_NAME);
 
-//Checkout and stash re source
+//Checkout and stash re source (stored on Master's local git repo)
 stage('Checkout'){
     node('master'){
         dir(git_url + "/" + PROJECT_NAME){
@@ -22,20 +23,17 @@ stage('Checkout'){
     }
 }
 
+//Construct build map for all nodes which should build
 def step_build = [:]
-
 for(n in re_nodes){
     def node_name = n
     step_build[node_name] = {
         node(node_name){
-            dir("${RE_PATH}"){
+            dir('${RE_PATH}'){
                 deleteDir()
                 unstash "source_code"
                 dir("build"){
-                    //Build the entire project 
-                    def result = utils.buildProject("Unix Makefiles", "")
-                    
-
+                    def result = utils.buildProject("Unix Makefiles", "-DMAKEFLAGS=-j6")
                     if(!result){
                         error('Failed to compile')
                     }
