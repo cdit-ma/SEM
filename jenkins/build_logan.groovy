5f95b4b0d1ade13e6b7bdbc9b6fad19f074363da
mitchell-conrad <mitchell.w.conrad@gmail.com>
2017-11-07 11:01:51 +1030

    Remove unused testing code.

--- jenkins/build_logan.groovy
+++ jenkins/build_logan.groovy
@@ -30,67 +30,8 @@ for(n in re_nodes){
             }
         }
     }
-
-/*
-    step_test[node_name] = {
-        node(node_name){
-            dir(PROJECT_NAME + "/bin/test"){
-                def globstr = "*"
-
-                //Find all executables
-                def test_list = findFiles glob: globstr
-
-                def test_count = 0;
-                def test_error_count = 0;
-
-                dir("results"){
-                    for(def file : test_list){
-                        def file_path = file.name
-                        def file_name = utils.trimExtension(file_path)
-                        def test_output = file_name + "_" + node_name + ".xml"
-                        print("Running Test: " + file_path)
-                        def test_error_code = utils.runScript("../" + file_path + " --gtest_output=xml:" + test_output)
-
-                        if(test_error_code != 0){
-                            test_error_count ++
-                        }
-                    }
-                    stash includes: "*.xml", name: node_name + "_test_cases"
-                }
-                //Clean up the directory after
-                deleteDir()
-            }
-        }
-    }
-    */
 }
 
 stage("Build"){
     parallel step_build_test
 }
-/*
-stage("Test"){
-    parallel step_test
-}
-
-node("master"){
-    dir("test_cases"){
-        for(n in re_nodes){
-            unstash(n + "_test_cases")
-        }
-
-        def globstr = "**.xml"
-        def test_results = findFiles glob: globstr
-        for (int i = 0; i < test_results.size(); i++){
-            def file_path = test_results[i].name
-            junit file_path
-        }
-        
-        //Test cases
-        def test_archive = "test_results.zip"
-        zip glob: globstr, zipFile: test_archive
-        archiveArtifacts test_archive
-        deleteDir()
-    }
-}
-*/
