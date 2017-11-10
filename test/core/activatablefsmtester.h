cd9cdc45c6bcf9ad8f47dfbff5e03869e5d5773b
danielrfraser <daniel.raymond.fraser@gmail.com>
2017-11-10 17:45:21 +1030

    Updated and fixed testing framework to check and test FSM contained in Activatable, moved to shared pointers inside RE

--- test/core/activatablefsmtester.h
+++ test/core/activatablefsmtester.h
@@ -3,6 +3,8 @@
 
 #include <random>
 
+
+
 std::string get_long_test_name(){
     auto test_info = testing::UnitTest::GetInstance()->current_test_info();
     return test_info->test_case_name() + std::string("_") + test_info->name();
@@ -18,7 +20,7 @@ class ActivatableFSMTester : public ::testing::Test {
     protected:
         Activatable* a = 0;
         std::default_random_engine random_generator;
-        
+
         virtual void SetUp(){
             random_generator.seed(testing::UnitTest::GetInstance()->random_seed());
         }
--- test/core/periodicevent/periodiceventtest.cpp
+++ test/core/periodicevent/periodiceventtest.cpp
@@ -1,42 +1,36 @@
-#include <iostream>
-#include <ostream>
-#include <core/eventports/periodiceventport.h>
-#include <core/component.h>
 #include "gtest/gtest.h"
 
+#include <core/eventports/periodiceventport.h>
+
+//Include the FSM Tester
+#include "../activatablefsmtester.h"
 
 
+void empty_callback(BaseMessage* b){};
 
-class StateMachineTest : public ::testing::Test {
-protected:
-    void SetUp(){
-        auto p = new PeriodicEventPort(nullptr, "PeriodicEvent", [](BaseMessage*){});
-        p->SetFrequency(1);
-        a = p;
-        ASSERT_TRUE(a);
-    }
-    void TearDown(){
-        if(a){
-            delete a;
-        }
-    }
-    void sleep_ms(int ms){
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-    };
-    
-    int random_sleep_ms(int lower_bound, int upper_bound){
-       // std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
-        //auto val = distribution(random_generator);
-        sleep_ms(1);
-        return 1;
-    }
-
-    Activatable* a = 0;
-    //std::default_random_engine random_generator(Testing::Test::random_seed());
+class PeriodicEventPort_0hz_FSMTester : public ActivatableFSMTester{
+    protected:
+        void SetUp(){
+            ActivatableFSMTester::SetUp();
+            auto port_name = get_long_test_name();
+            auto port = new PeriodicEventPort(nullptr, port_name, empty_callback);
+            port->SetFrequency(0);
+            a = port;
+            ASSERT_TRUE(a);
+        };
 };
 
-#define MACRO_CLASS_NAME StateMachineTest
-#include "../../core/state_machine_tests.h"
+class PeriodicEventPort_1hz_FSMTester : public ActivatableFSMTester{
+    protected:
+        void SetUp(){
+            ActivatableFSMTester::SetUp();
+            auto port_name = get_long_test_name();
+            auto port = new PeriodicEventPort(nullptr, port_name, empty_callback);
+            port->SetFrequency(1);
+            a = port;
+            ASSERT_TRUE(a);
+        };
+};
 
 
 
@@ -44,6 +38,7 @@ protected:
 struct PeriodTestCase{
     PeriodTestCase(const int test_time_ms, const double periodic_hz, const double workload_perc, const int expected_ticks, const double error_confidence = 0.95){
         this->test_time_ms = test_time_ms;
+        
         this->periodic_hz = periodic_hz;
         this->expected_ticks = expected_ticks;
         this->error_confidence = error_confidence;
@@ -79,7 +74,7 @@ class PeriodicEventTest : public ::testing::TestWithParam<PeriodTestCase>{};
 //This is our test case for the frame
 TEST_P(PeriodicEventTest, TickCount)
 {
-    //Get our test structure 
+    //Get our test structure
     auto p = GetParam();
 
     RecordProperty("test_time_ms", p.test_time_ms);
@@ -89,31 +84,31 @@ TEST_P(PeriodicEventTest, TickCount)
 
    int callback_tick_count = 0;
    {
-       auto pe = std::make_shared<PeriodicEventPort>(nullptr, "PeriodicEvent", [&callback_tick_count, p](BaseMessage* m){
+       PeriodicEventPort port(nullptr, "PeriodicEvent", [&callback_tick_count, p](BaseMessage* m){
                std::this_thread::sleep_for(std::chrono::milliseconds(p.callback_time_ms));
                callback_tick_count ++;
            });
 
        //Set the frequency, and startup the Periodic Event
-       pe->SetFrequency(p.periodic_hz);
-       pe->Configure();
-       pe->Activate();
+       port.SetFrequency(p.periodic_hz);
+       EXPECT_TRUE(port.Configure());
+       EXPECT_TRUE(port.Activate());
 
        //Run for the desired test length
        std::this_thread::sleep_for(std::chrono::milliseconds(p.test_time_ms));
-       pe->Passivate();
-       pe->Terminate();
+       EXPECT_TRUE(port.Passivate());
+       EXPECT_TRUE(port.Terminate());
 
-       auto total_rxd = pe->GetEventsReceieved();
-       auto proc_rxd = pe->GetEventsProcessed();
+       auto total_rxd = port.GetEventsReceieved();
+       auto proc_rxd = port.GetEventsProcessed();
 
        //Check that we got the same number of proccessed rx messages as we did in our callback
        EXPECT_EQ(proc_rxd, callback_tick_count);
    }
 
    //The number of callbacks we got should fall within the range of satisfactory
-   ASSERT_GE(callback_tick_count, p.lower);
-   ASSERT_LE(callback_tick_count, p.upper);
+   EXPECT_GE(callback_tick_count, p.lower);
+   EXPECT_LE(callback_tick_count, p.upper);
 }
 
 //Define a helper to generate a range of test cases for a particular hz/time/confidence interval
@@ -133,26 +128,22 @@ std::vector<PeriodTestCase> getTestCases(int hz, double time, double confidence_
     return test_cases;
 };
 
-//Define a helper to generate a range of test cases for a particular hz/time/confidence interval
-std::vector<Activatable*> getTypes() 
-{
-    std::vector<Activatable*> test_cases;
 
-    auto a = new PeriodicEventPort(nullptr, "PeriodicEvent", nullptr);
-    test_cases.push_back(a);
-    return test_cases;
-};
+#define TEST_FSM_CLASS PeriodicEventPort_1hz_FSMTester
+#include "../activatablefsmtestcases.h"
+#undef TEST_FSM_CLASS
 
-//INSTANTIATE_TEST_CASE_P(PEP, StateMachineTest, ::testing::ValuesIn(getTypes()));
-//INSTANTIATE_TEST_CASE_P(1Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(1, 5)));
-/*
+#define TEST_FSM_CLASS PeriodicEventPort_0hz_FSMTester
+#include "../activatablefsmtestcases.h"
+#undef TEST_FSM_CLASS
+
+INSTANTIATE_TEST_CASE_P(0Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(0, 5, 1)));
+INSTANTIATE_TEST_CASE_P(1Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(1, 5)));
 INSTANTIATE_TEST_CASE_P(2Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(2, 5)));
 INSTANTIATE_TEST_CASE_P(4Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(4, 5)));
 INSTANTIATE_TEST_CASE_P(8Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(8, 5)));
 INSTANTIATE_TEST_CASE_P(16Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(16, 5)));
 INSTANTIATE_TEST_CASE_P(32Hz_5s, PeriodicEventTest, ::testing::ValuesIn(getTestCases(32, 5, 0.70)));
-*/
-
 
 int main(int ac, char* av[])
 {
