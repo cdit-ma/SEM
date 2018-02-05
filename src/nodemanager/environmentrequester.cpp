0327a75f2069f4f51c578beb22ad05cca0f6d53e
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-02-05 16:22:42 +1030

    Add environmentrequester implementation.

--- src/nodemanager/environmentrequester.cpp
+++ src/nodemanager/environmentrequester.cpp
@@ -1,8 +1,30 @@
 #include "environmentrequester.h"
 
-EnvironmentRequester::EnvironmentRequester(const std::string& deployment_id, const std::string& deployment_info){
+EnvironmentRequester::EnvironmentRequester(const std::string& manager_address, 
+                                            const std::string& deployment_id, 
+                                            const std::string& deployment_info){
+    manager_address_ = manager_address;
     deployment_id_ = deployment_id;
     deployment_info_ = deployment_info;
+}
+
+void EnvironmentRequester::Init(){
+    try{
+        context_ = new zmq::context_t(1);
+        zmq::socket_t sub(*context_, ZMQ_SUB);
+        sub.connect(manager_address_);
+        zmq::message_t message;
+        sub.recv(&message);
+        manager_endpoint_ = std::string(static_cast<const char*>(message.data()), message.size());
+        sub.close();
+    }
+    catch(std::exception& ex){
+        std::cout << ex.what() << " in EnvironmentRequester::Init" << std::endl;
+    }
+}
+
+void EnvironmentRequester::Start(){
+    environment_comms_thread_ = new std::thread(&EnvironmentRequester::HeartbeatLoop, this);
 }
 
 long EnvironmentRequester::Tick(){
@@ -21,23 +43,76 @@ long EnvironmentRequester::GetClock(){
     return clock_;
 }
 
-void EnvironmentRequester::Start(){
+void EnvironmentRequester::HeartbeatLoop(){
+    zmq::socket_t* initial_request_socket = new zmq::socket_t(*context_, ZMQ_REQ);
+    initial_request_socket->connect(manager_endpoint_);
 
+    ZMQSendTwoPartRequest(initial_request_socket, "DEPLOYMENT", deployment_id_);
+
+    auto reply = ZMQReceiveTwoPartReply(initial_request_socket);
+
+    if(reply.reply_type_.compare("SUCCESS") == 0){
+        manager_update_endpoint_ = reply.reply_data_;
+    }
+    else{
+        //TODO: Handle this error
+        std::cout << "HANDLE THIS ERROR! 1" << std::endl;
+    }
+    zmq::socket_t* update_socket;
+    try{
+        update_socket = new zmq::socket_t(*context_, ZMQ_REQ);
+        update_socket->connect(manager_update_endpoint_);
+    }
+    catch(std::exception& ex){
+        std::cout << ex.what() << " in EnvironmentRequester::Start" << std::endl;
+    }
+
+
+    //TODO: Contidtion var to break out
+    while(true){
+        if(request_queue_.empty()){
+            std::unique_lock<std::mutex> lock(request_queue_lock_);
+            auto trigger = request_queue_cv_.wait_for(lock, std::chrono::duration<int>(heartbeat_period_));
+
+            if(trigger == std::cv_status::timeout){
+                ZMQSendTwoPartRequest(update_socket, "HEARTBEAT", "");
+                auto reply = ZMQReceiveTwoPartReply(update_socket);
+            }
+        }
+
+        else{
+            //Pull update off queue and send
+            auto request = request_queue_.front();
+            request_queue_.pop();
+
+            ZMQSendTwoPartRequest(update_socket, request.request_type_, request.request_data_);
+            auto reply = ZMQReceiveTwoPartReply(update_socket);
+
+            //Handle response
+            if(reply.reply_type_.compare("SUCCESS") == 0){
+                request.response_->set_value(reply.reply_data_);
+            }
+            else{
+                //TODO: Handle this failure/other response
+            }
+        }
+    }
 }
 
 void EnvironmentRequester::End(){
 
 }
 
-
 int EnvironmentRequester::GetPort(const std::string& component_id, const std::string& component_info){
-
     int port = 0;
+    auto port_response = SendRequest("ASSIGNMENT_REQUEST", component_id);
 
-    auto response = SendRequest("ASSIGNMENT_REQUEST", component_id);
-
-    port = std::stoi(response.get());
-
+    try{
+        port = std::stoi(port_response.get());
+    }
+    catch(std::exception& ex){
+        std::cout << ex.what() << " in EnvironmentRequester::GetPort" << std::endl;
+    }
     return port;
 }
 
@@ -54,3 +129,42 @@ std::future<std::string> EnvironmentRequester::SendRequest(const std::string& re
     return request_future;
 }
 
+void EnvironmentRequester::ZMQSendTwoPartRequest(zmq::socket_t* socket, const std::string& request_type, 
+                                                                        const std::string& request){
+    std::string lamport_string = std::to_string(Tick());
+    zmq::message_t lamport_time_msg(lamport_string.begin(), lamport_string.end());
+    zmq::message_t request_type_msg(request_type.begin(), request_type.end());
+    zmq::message_t request_msg(request.begin(), request.end());
+
+    try{
+        socket->send(request_type_msg, ZMQ_SNDMORE);
+        socket->send(lamport_time_msg, ZMQ_SNDMORE);
+        socket->send(request_msg);
+    }
+    catch(std::exception error){
+        std::cout << error.what() << "in EnvironmentRequester::SendTwoPartRequest" << std::endl;
+    }
+}
+
+EnvironmentRequester::Reply EnvironmentRequester::ZMQReceiveTwoPartReply(zmq::socket_t* socket){
+    zmq::message_t request_type_msg;
+    zmq::message_t lamport_time_msg;
+    zmq::message_t request_contents_msg;
+    try{
+        socket->recv(&request_type_msg);
+        socket->recv(&lamport_time_msg);
+        socket->recv(&request_contents_msg);
+    }
+    catch(zmq::error_t error){
+        //TODO: Throw this further up
+        std::cout << error.what() << std::endl;
+    }
+    std::string type(static_cast<const char*>(request_type_msg.data()), request_type_msg.size());
+    std::string contents(static_cast<const char*>(request_contents_msg.data()), request_contents_msg.size());
+
+    //Update and get current lamport time
+    std::string incoming_time(static_cast<const char*>(lamport_time_msg.data()), lamport_time_msg.size());
+    long lamport_time = SetClock(std::stol(incoming_time));
+
+    return Reply{type, contents};
+}
--- src/nodemanager/environmentrequester.h
+++ src/nodemanager/environmentrequester.h
@@ -6,6 +6,7 @@
 #include <thread>
 #include <queue>
 #include <future>
+#include <zmq.hpp>
 
 class EnvironmentRequester{
 
@@ -16,17 +17,28 @@ class EnvironmentRequester{
         std::promise<std::string>* response_;
     };
 
+    struct Reply{
+        std::string reply_type_;
+        std::string reply_data_;
+    };
+
     public:
-        EnvironmentRequester(const std::string& deployment_id, const std::string& deployment_info);
+        EnvironmentRequester(const std::string& manager_address,
+                                const std::string& deployment_id,
+                                const std::string& deployment_info);
+        void Init();
         void Start();
+        void HeartbeatLoop();
         void End();
 
         int GetPort(const std::string& component_id, const std::string& component_info);
 
     private:
+        std::string manager_address_;
+        std::string manager_endpoint_;
+        std::string manager_update_endpoint_;
         std::future<std::string> SendRequest(const std::string& request_type, const std::string& request);
 
-        std::string environment_manager_addr_;
         std::string deployment_id_;
         std::string deployment_info_;
 
@@ -36,11 +48,16 @@ class EnvironmentRequester{
         long SetClock(long incoming_time);
         long GetClock();
 
-        std::thread environment_comms_thread_;
+        std::thread* environment_comms_thread_;
         std::queue<Request> request_queue_;
         std::mutex request_queue_lock_;
         std::condition_variable request_queue_cv_;
+        int heartbeat_period_ = 2;
 
+        zmq::context_t* context_;
+
+        void ZMQSendTwoPartRequest(zmq::socket_t* socket, const std::string& request_type, const std::string& request);
+        Reply ZMQReceiveTwoPartReply(zmq::socket_t* socket);
 };
 
 #endif //NODEMANAGER_ENVIRONMENTREQUESTER_H
