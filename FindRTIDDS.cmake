ddc0f3a4944b811d38a3fe842e1a59c937802d9f
danielrfraser <danielrfraser@github.com>
2018-03-08 12:59:16 +1030

    Updated and added TAO find CMAKE file RE-170

--- FindRTIDDS.cmake
+++ FindRTIDDS.cmake
@@ -1,142 +1,116 @@
-# Find a DDS implementation.
+# Find an RTI DDS implementation.
 # Currently only RTI's DDS implementation is searched for.
 #
-# The following directories are searched:
-# DDS_ROOT (CMake variable)
-# DDS_ROOT (Environment variable)
+# The following directories are used to locate RTI:
+# RTI_DDS_ROOT (CMake variable)
 # NDDSHOME (Environment variable)
+# To find the correct binaries, we also need to know the target of the host binaries:
+# RTI_DDS_HOST (CMake variable)
+# RTI_DDS_HOST (Environment variable)
 #
-# Prior to calling this script, you must set the RTI_DDS_HOST variable. This is used
-# when searching for some implementations as a name for the directory
-# containing the library files. For example, it could be set to
-# "x64Linux2.6gcc4.1.1".
-#
-# This sets the following variables:
-# DDS_FOUND - True if DDS was found.
-# DDS_VENDOR - Name of the DDS vendor found (e.g. "RTI")
-# DDS_INCLUDE_DIRS - Directories containing the DDS include files.
-# DDS_LIBRARIES - Libraries needed to use DDS.
-# DDS_DEFINITIONS - Compiler flags for DDS.
-# DDS_VERSION - The version of DDS found.
-# DDS_VERSION_MAJOR - The major version of DDS found.
-# DDS_VERSION_MINOR - The minor version of DDS found.
-# DDS_VERSION_PATCH - The revision version of DDS found.
-# DDS_VERSION_CAN - The candidate version of DDS found.
+# ie:
+# SET(RTI_DDS_ROOT "/opt/RTI/rti_connext_dds-5.3.0")
+# SET(RTI_DDS_HOST "x64Linux2.6gcc4.1.1")
 
+if(NOT RTI_DDS_ROOT)
+    # If RTI_DDS_ROOT has not been set in CMAKE, use the environment variable NDDSHOME
+    SET(RTI_DDS_ROOT $ENV{NDDSHOME})
+endif()
 
-
-function(RTI_GENERATE_CPP SRCS HDRS)
-    #Copy the values from SRCS and HDRS into src,hdrs
-    set(${SRCS} ${${SRCS}})
-    set(${HDRS} ${${HDRS}})
-
-    foreach(FIL ${ARGN})
-        # Copy IDL into binary directory
-        configure_file(${FIL} ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
-        
-        get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
-        get_filename_component(FIL_WE ${FIL} NAME_WE)
-
-        list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.cxx")
-        list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.hpp")
-        #The below are needed if we use -stl
-        #list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.cxx")
-        #list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.hpp")
-        
-        #The below are needed if we don't use -stl
-        list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.cxx")
-        list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.cxx")
-        list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.h")
-        list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.h")
-
-        add_custom_command(
-        OUTPUT  "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.cxx"
-                "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.hpp"
-                #"${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.cxx"
-                #"${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.hpp"
-                "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.cxx"
-                "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.cxx"
-                "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.h"
-                "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.h"
-        COMMAND  ${RTIDDS_GEN_EXECUTABLE}
-        ARGS -language C++11 -unboundedSupport -namespace -update typefiles -d ${CMAKE_CURRENT_BINARY_DIR} ${ABS_FIL}
-        DEPENDS ${ABS_FIL} ${RTIDDS_GEN_EXECUTABLE} 
-        COMMENT "Running C++ rtiddsgen on ${FIL}"
-        VERBATIM )
-    endforeach()
-
-    set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
-    set(${SRCS} ${${SRCS}} PARENT_SCOPE)
-    set(${HDRS} ${${HDRS}} PARENT_SCOPE)
-endfunction()
-
-find_path(DDS_INCLUDE_DIR ndds/ndds_cpp.h
-    HINTS ${DDS_ROOT}/include $ENV{DDS_ROOT}/include
-    $ENV{NDDSHOME}/include)
-
-
+if(NOT RTI_DDS_ROOT)
+    message(STATUS "Error: Either RTI_DDS_ROOT or NDDSHOME are not set, have you run the RTI configure script?")
+    return()
+endif()
 
 if(NOT RTI_DDS_HOST)
+    # If RTI_DDS_HOST has not been set in CMAKE, use the environment variable RTI_DDS_HOST
     SET(RTI_DDS_HOST $ENV{RTI_DDS_HOST})
 endif()
 
 if(NOT RTI_DDS_HOST)
-    message(STATUS "Error: RTI_DDS_HOST not set as an Environment Variable")
+    message(STATUS "Error: Variable RTI_DDS_HOST not set.")
     return()
 endif()
 
-find_library(DDS_C_LIBRARY nddsc
-    HINTS ${DDS_ROOT}/lib $ENV{DDS_ROOT}/lib $ENV{NDDSHOME}/lib
-    PATH_SUFFIXES ${RTI_DDS_HOST})
-find_library(DDS_CPP_LIBRARY nddscpp
-    HINTS ${DDS_ROOT}/lib $ENV{DDS_ROOT}/lib $ENV{NDDSHOME}/lib
-    PATH_SUFFIXES ${RTI_DDS_HOST})
+find_path(RTI_DDS_INCLUDE_DIR NAMES ndds/ndds_cpp.h HINTS ${RTI_DDS_ROOT}/include)
 
-find_library(DDS_CPP2_LIBRARY nddscpp2
-    HINTS ${DDS_ROOT}/lib $ENV{DDS_ROOT}/lib $ENV{NDDSHOME}/lib
-    PATH_SUFFIXES ${RTI_DDS_HOST})
+find_library(RTI_DDS_C_LIBRARY NAMES nddsc HINTS ${RTI_DDS_ROOT}/lib PATH_SUFFIXES ${RTI_DDS_HOST})
+find_library(RTI_DDS_CPP_LIBRARY NAMES nddscpp HINTS ${RTI_DDS_ROOT}/lib PATH_SUFFIXES ${RTI_DDS_HOST})
+find_library(RTI_DDS_CPP2_LIBRARY NAMES nddscpp2 HINTS ${RTI_DDS_ROOT}/lib PATH_SUFFIXES ${RTI_DDS_HOST})
+find_library(RTI_DDS_CORE_LIBRARY NAMES nddscore HINTS ${RTI_DDS_ROOT}/lib PATH_SUFFIXES ${RTI_DDS_HOST})
 
-find_library(DDS_CORE_LIBRARY nddscore
-    HINTS ${DDS_ROOT}/lib $ENV{DDS_ROOT}/lib $ENV{NDDSHOME}/lib
-    PATH_SUFFIXES ${RTI_DDS_HOST})
-
-find_program(RTIDDS_GEN_EXECUTABLE
-        NAMES rtiddsgen
-        DOC "The RTIDDS idl compiler"
-        PATHS
-        $ENV{NDDSHOME}/bin)
+find_program(RTI_DDS_GEN_EXECUTABLE
+    NAMES rtiddsgen
+    DOC "The RTIDDS idl compiler"
+    HINTS
+    $ENV{NDDSHOME}/bin)
 
 if(WIN32)
-    set(RTIDDS_GEN_EXECUTABLE $ENV{NDDSHOME}/bin/rtiddsgen.bat)
-    set(DDS_EXTRA_LIBRARIES netapi32 advapi32 user32 ws2_32)
-    set(DDS_DEFINITIONS "-DRTI_WIN32 -DNDDS_DLL_VARIABLE /MD")
+    set(RTI_DDS_GEN_EXECUTABLE ${RTI_DDS_ROOT}/bin/rtiddsgen.bat)
+    set(RTI_DDS_EXTRA_LIBRARIES netapi32 advapi32 user32 ws2_32)
+    set(RTI_DDS_DEFINITIONS "-DRTI_WIN32 -DNDDS_DLL_VARIABLE /MD")
 elseif(UNIX AND NOT APPLE)
-    set(DDS_DEFINITIONS "-DRTI_UNIX -DRTI_64BIT")
-    set(DDS_EXTRA_LIBRARIES dl nsl m pthread rt)
+    set(RTI_DDS_DEFINITIONS "-DRTI_UNIX -DRTI_64BIT")
+    set(RTI_DDS_EXTRA_LIBRARIES dl nsl m pthread rt)
 elseif(APPLE)
-    set(DDS_DEFINITIONS "-DRTI_UNIX -DRTI_64BIT")
+    set(RTI_DDS_DEFINITIONS "-DRTI_UNIX -DRTI_64BIT")
 endif(WIN32)
 
-set(RTIDDS_INCLUDE_DIRS ${DDS_INCLUDE_DIR} ${DDS_INCLUDE_DIR}/ndds ${DDS_INCLUDE_DIR}/ndds/hpp)
-set(RTIDDS_LIBRARIES ${DDS_C_LIBRARY} ${DDS_CPP_LIBRARY} ${DDS_CORE_LIBRARY}
-    ${DDS_EXTRA_LIBRARIES} ${DDS_CPP2_LIBRARY})
-set(RTIDDS_DEFINITIONS ${DDS_DEFINITIONS})
-
-
-file(GLOB DDS_VERSION_FILE ${DDS_ROOT}/rev_target_rtidds.*
-    $ENV{DDS_ROOT}/rev_target_rtidds.*
-    $ENV{NDDSHOME}/rev_target_rtidds.*)
-if(DDS_VERSION_FILE)
-    string(REGEX MATCH "rtidds.([0-9]+).([0-9]+)([a-z]*)"
-        DDS_VERSION "${DDS_VERSION_FILE}")
-    set(DDS_VERSION_MAJOR ${CMAKE_MATCH_1})
-    set(DDS_VERSION_MINOR ${CMAKE_MATCH_2})
-    set(DDS_VERSION_PATCH ${CMAKE_MATCH_3})
-    set(DDS_VERSION_CAN)
-endif(DDS_VERSION_FILE)
+set(RTI_DDS_INCLUDE_DIRS ${RTI_DDS_INCLUDE_DIR} ${RTI_DDS_INCLUDE_DIR}/ndds ${RTI_DDS_INCLUDE_DIR}/ndds/hpp)
+set(RTI_DDS_LIBRARIES ${RTI_DDS_C_LIBRARY} ${RTI_DDS_CPP_LIBRARY} ${RTI_DDS_CPP2_LIBRARY} ${RTI_DDS_CORE_LIBRARY} ${RTI_DDS_EXTRA_LIBRARIES})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(RTIDDS
-    REQUIRED_VARS DDS_INCLUDE_DIR DDS_C_LIBRARY DDS_CPP_LIBRARY
-    DDS_CORE_LIBRARY
-    VERSION_VAR ${DDS_VERSION})
+    REQUIRED_VARS RTI_DDS_INCLUDE_DIRS RTI_DDS_LIBRARIES RTI_DDS_GEN_EXECUTABLE)
+
+
+function(RTI_GENERATE_CPP SRCS HDRS)
+    #Copy the values from variables defined by SRCS/HDRS into SOURCES/HEADERS respectively
+    set(SOURCES ${${SRCS}})
+    set(HEADERS ${${HDRS}})
+
+    foreach(FILE ${ARGN})
+        # Copy IDL into binary directory
+        configure_file(${FILE} ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
+        get_filename_component(ABS_FILE ${FILE} ABSOLUTE)
+        get_filename_component(FIL_WE ${FILE} NAME_WE)
+
+        set(CURRENT_SRCS 
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.cxx"
+            # RTI 5.3.0
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.cxx"
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.cxx"
+            # RTI 5.2.3
+            #"${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.cxx"
+            )
+
+        set(CURRENT_HDRS
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.hpp"
+            # RTI 5.3.0
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Impl.h"
+            "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}ImplPlugin.h"
+            # RTI 5.2.3
+            #"${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}Plugin.hpp"
+        )
+
+        # Append the current srcs/headers into the big list
+        list(APPEND SOURCES "${CURRENT_SRCS}")
+        list(APPEND HEADERS "${CURRENT_HDRS}")
+        
+        add_custom_command(
+            OUTPUT  ${CURRENT_SRCS}
+                    ${CURRENT_HDRS}
+            COMMAND ${RTI_GEN_EXECUTABLE}
+            ARGS -language C++11 -unboundedSupport -namespace -update typefiles -d ${CMAKE_CURRENT_BINARY_DIR} ${ABS_FILE}
+            DEPENDS ${ABS_FILE} ${RTI_GEN_EXECUTABLE} 
+            COMMENT "Running C++ rtiddsgen on ${FILE}"
+            VERBATIM)
+
+        #Set the File properties for the generated files
+        set_source_files_properties(${CURRENT_SRCS} ${CURRENT_HDRS} PROPERTIES GENERATED TRUE)
+    endforeach()
+    
+    #Append the source/header files generated into the vars labelled as the parameters passed into the function
+    set(${SRCS} ${SOURCES} PARENT_SCOPE)
+    set(${HDRS} ${HEADERS} PARENT_SCOPE)
+endfunction()
