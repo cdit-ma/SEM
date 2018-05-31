9273b71f27daea58cc861746c87cca9f409c0c10
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-05-31 13:06:48 +0930

    Tidy up command line arguments.

--- src/nodemanager/nodemanager.cpp
+++ src/nodemanager/nodemanager.cpp
@@ -33,32 +33,31 @@ int main(int argc, char **argv){
     std::string environment_manager_endpoint;
     std::string experiment_id;
     bool live_logging = false;
+    std::string address;
 
     //Master arguments
-    std::string master_endpoint;
     std::string graphml_path;
     double execution_duration = 60.0;
 
     //Slave arguments
     std::string dll_path;
-    std::string slave_endpoint;
 
     boost::program_options::options_description options("Node manager options");
     //Add shared arguments
-    options.add_options()("live-logging,L", boost::program_options::value<bool>(&live_logging), "Master endpoint, including port");
-    options.add_options()("environment-manager,e", boost::program_options::value<std::string>(&environment_manager_endpoint), "Environment manager endpoint.");
+    options.add_options()("address,a", boost::program_options::value<std::string>(&address), "Node manager ip address.");
     options.add_options()("experiment-id,n", boost::program_options::value<std::string>(&experiment_id), "ID of experiment to start.");
-    options.add_options()("help,h", "Display help");
-
+    options.add_options()("environment-manager,e", boost::program_options::value<std::string>(&environment_manager_endpoint), "Environment manager fully qualified endpoint ie. (tcp://192.168.111.230:20000).");
+    options.add_options()("live-logging,L", boost::program_options::value<bool>(&live_logging), "Live model logging toggle.");
+    
     //Add master arguments
-    options.add_options()("deployment,d", boost::program_options::value<std::string>(&graphml_path), "Deployment graphml file path");
-    options.add_options()("master,m", boost::program_options::value<std::string>(&master_endpoint), "Master endpoint, including port");
+    options.add_options()("deployment,d", boost::program_options::value<std::string>(&graphml_path), "Deployment graphml file path.");
     options.add_options()("time,t", boost::program_options::value<double>(&execution_duration)->default_value(execution_duration), "Deployment Duration (In Seconds)");
 
     //Add slave arguments
-    options.add_options()("library,l", boost::program_options::value<std::string>(&dll_path), "Library path");
-    options.add_options()("slave,s", boost::program_options::value<std::string>(&slave_endpoint), "Slave endpoint, including port");
+    options.add_options()("library,l", boost::program_options::value<std::string>(&dll_path), "Model generated library path.");
 
+    //Add other options
+    options.add_options()("help,h", "Display help");
 
     //Construct a variable_map
     boost::program_options::variables_map vm;
@@ -90,25 +89,21 @@ int main(int argc, char **argv){
         std::cerr << "Arg Error: re_node_manager requires a valid experiment-id." << std::endl;
         valid_args = false;
     }
+    if(address.empty()){
+        std::cerr << "Arg Error: re_node_manager requires a valid ip address." << std::endl;
+        valid_args = false;
+    }
 
-    bool is_master = !master_endpoint.empty() || !graphml_path.empty();
+    bool is_master = !graphml_path.empty();
     if(is_master){
-        if(master_endpoint.empty()){
-            std::cerr << "Arg Error: re_node_manager[Master] requires a valid endpoint." << std::endl;
-            valid_args = false;
-        }
         if(graphml_path.empty()){
             std::cerr << "Arg Error: re_node_manager[Master] requires a graphml deployment to execute." << std::endl;
             valid_args = false;
         }
     }
 
-    bool is_slave = !slave_endpoint.empty() || !dll_path.empty();
+    bool is_slave = !dll_path.empty();
     if(is_slave){
-        if(slave_endpoint.empty()){
-            std::cerr << "Arg Error: re_node_manager[Slave] requires a valid endpoint." << std::endl;
-            valid_args = false;
-        }
         if(dll_path.empty()){
             std::cerr << "Arg Error: re_node_manager[Slave] requires a library path to execute." << std::endl;
             valid_args = false;
@@ -122,13 +117,13 @@ int main(int argc, char **argv){
 
     if(!valid_args){
         std::cout << options << std::endl;
-		return 1;
+        return 1;
     }
 
     std::cout << "-------[" + VERSION_NAME +" v" + RE_VERSION + "]-------" << std::endl;
     if(is_master){
         std::cout << "* Master:" << std::endl;
-        std::cout << "** Endpoint: " << master_endpoint << std::endl;
+        std::cout << "** Endpoint: " << address << std::endl;
         std::cout << "** Deployment Graphml: " << graphml_path << std::endl;
         if(execution_duration == -1){
             std::cout << "** Duration: " << "Indefinite" << std::endl;
@@ -138,45 +133,37 @@ int main(int argc, char **argv){
     }
     if(is_slave){
         std::cout << "* Slave:" << std::endl;
-        std::cout << "** Endpoint: " << slave_endpoint << std::endl;
+        std::cout << "** Endpoint: " << address << std::endl;
         std::cout << "** Library Path: " << dll_path << std::endl;
     }
 
-    zmq::Registrar* master = 0;
-    zmq::Registrant* slave = 0;
 
+    std::unique_ptr<zmq::Registrar> master;
+    std::unique_ptr<zmq::Registrant> slave;
     bool success = true;
 
     if(success && is_master){
-        auto execution_manager = new ExecutionManager(master_endpoint,
+        auto execution_manager = new ExecutionManager(address,
                                                     graphml_path,
                                                     execution_duration,
                                                     exe,
                                                     experiment_id,
                                                     environment_manager_endpoint);
 
-        master = new zmq::Registrar(execution_manager, master_endpoint);
+        master = std::unique_ptr<zmq::Registrar>(new zmq::Registrar(execution_manager, address));
 
         success = execution_manager->IsValid();
     }
 
     if(success && is_slave){
         //Construct a Deployment Manager to handle the Deployment
-        auto deployment_manager = new DeploymentManager(is_master, dll_path, exe, experiment_id, slave_endpoint, environment_manager_endpoint);
-        slave = new zmq::Registrant(deployment_manager);
+        auto deployment_manager = new DeploymentManager(is_master, dll_path, exe, experiment_id, address, environment_manager_endpoint);
+        slave = std::unique_ptr<zmq::Registrant>(new zmq::Registrant(deployment_manager));
     }
 
     if(success){
         //Use execution class to wait for interrupt
         exe->Start();
-    }
-
-    if(slave){
-        delete slave;
-    }
-
-    if(master){
-        delete master;
     }
 
     delete exe;
