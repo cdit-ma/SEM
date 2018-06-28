2b3e8019aafb51a8bbdd760000d324249e7b063e
danielrfraser <daniel.raymond.fraser@gmail.com>
2018-06-28 13:37:56 +0930

    Patched and changed GENERATE_REGEN to use new changes to re_gen

--- FindREGEN.cmake
+++ FindREGEN.cmake
@@ -1,64 +1,48 @@
-
 find_package(Java COMPONENTS Runtime)
 include(UseJava)
 include(FindPackageHandleStandardArgs)
 
-SET(DATATYPES_XSL "g2datatypes.xsl")
-SET(COMPONENTS_XSL "g2components.xsl")
+SET(GENERATE_XSL "generate_project.xsl")
 
-function(REGEN_GENERATE_CPP REGEN_GENERATED_FOLDERS)
-    #Copy the values from SRCS and HDRS into src,hdrs
-    set(${REGEN_GENERATED_FOLDERS} ${${REGEN_GENERATED_FOLDERS}})
+function(REGEN_GENERATE_CPP)
+    set(oneValueArgs GRAPHML PROJECT_PREFIX GENERATED_DIRS)
+    cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "" ${ARGN})
+
+    if(NOT ARGS_GENERATED_DIRS)
+        message(SEND_ERROR "Error: REGEN_GENERATE_CPP() called without a GENERATED_DIRS")
+        return()
+    endif()
+
     set(WORKING_DIR ${CMAKE_CURRENT_BINARY_DIR})
+    
+    # Copy file into Binary dir
+    get_filename_component(ABS_FIL ${ARGS_GRAPHML} ABSOLUTE)
+    get_filename_component(GRAPHML_PATH ${ARGS_GRAPHML} NAME)
+    
+    execute_process(
+        COMMAND ${Java_JAVA_EXECUTABLE} -jar ${REGEN_PATH}/saxon.jar -xsl:${REGEN_PATH}/${GENERATE_XSL} -s:${ABS_FIL} library_prefix=${ARGS_PROJECT_PREFIX}
+        WORKING_DIRECTORY "${WORKING_DIR}/"
+        OUTPUT_QUIET
+        ERROR_VARIABLE GENERATE_ERROR
+        RESULT_VARIABLE GENERATE_SUCCESS
+        ERROR_STRIP_TRAILING_WHITESPACE
+    )
 
-    foreach(FILE ${ARGN})
-        # Copy file into Binary dir
-        configure_file(${FILE} ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
-        get_filename_component(ABS_FIL ${FILE} ABSOLUTE)
-        get_filename_component(GRAPHML_PATH ${FILE} NAME)
-        
-        set(GRAPHML_FILE ${CMAKE_CURRENT_BINARY_DIR}/${GRAPHML_PATH})
-        
-        execute_process(
-            COMMAND ${Java_JAVA_EXECUTABLE} -jar ${REGEN_PATH}/saxon.jar -xsl:${REGEN_PATH}/${DATATYPES_XSL} -s:${GRAPHML_FILE}
-            WORKING_DIRECTORY "${WORKING_DIR}/"
-            OUTPUT_QUIET
-            ERROR_VARIABLE DATATYPE_ERROR
-            RESULT_VARIABLE DATATYPE_SUCCESS
-            ERROR_STRIP_TRAILING_WHITESPACE
-        )
+    if(${GENERATE_SUCCESS} STREQUAL "0")
+        message(STATUS "RE_GEN: Generated C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
+    else()
+        message(SEND_ERROR "RE_GEN: Failed generating C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
+        message("--->  ${GENERATE_XSL} Error Output [START] <---")
+        message(${GENERATE_ERROR})
+        message("--->  ${GENERATE_XSL} Error Output  [END]  <---")
+        return()
+    endif()
+    list(APPEND ${ARGS_GENERATED_DIRS} "${WORKING_DIR}/classes")
+    list(APPEND ${ARGS_GENERATED_DIRS} "${WORKING_DIR}/components")
+    list(APPEND ${ARGS_GENERATED_DIRS} "${WORKING_DIR}/datatypes")
+    list(APPEND ${ARGS_GENERATED_DIRS} "${WORKING_DIR}/ports")
 
-        if(${DATATYPE_SUCCESS} STREQUAL "0")
-            message(STATUS "RE_GEN: Generated datatype C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
-        else()
-            message(SEND_ERROR "RE_GEN: Failed generating datatype C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
-            message("--->  ${DATATYPES_XSL} Error Output [START] <---")
-            message(${DATATYPE_ERROR})
-            message("--->  ${DATATYPES_XSL} Error Output  [END]  <---")
-        endif()
-
-        execute_process(
-            COMMAND ${Java_JAVA_EXECUTABLE} -jar ${REGEN_PATH}/saxon.jar -xsl:${REGEN_PATH}/${COMPONENTS_XSL} -s:${GRAPHML_FILE}
-            WORKING_DIRECTORY "${WORKING_DIR}/"
-            OUTPUT_QUIET
-            ERROR_VARIABLE COMPONENTS_ERROR
-            RESULT_VARIABLE COMPONENTS_SUCCESS
-            ERROR_STRIP_TRAILING_WHITESPACE
-        )
-
-        if(${COMPONENTS_SUCCESS} STREQUAL "0")
-            message(STATUS "RE_GEN: Generated component C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
-        else()
-            message(SEND_ERROR "RE_GEN: Failed generating component C++ for model: ${GRAPHML_PATH} in: ${WORKING_DIR}")
-            message("--->  ${COMPONENTS_XSL} Error Output [START] <---")
-            message(${COMPONENTS_ERROR})
-            message("--->  ${COMPONENTS_XSL} Error Output  [END]  <---")
-        endif()
-    endforeach()
-    list(APPEND ${REGEN_GENERATED_FOLDERS} "${WORKING_DIR}/datatypes")
-    list(APPEND ${REGEN_GENERATED_FOLDERS} "${WORKING_DIR}/components")
-
-    set(${REGEN_GENERATED_FOLDERS} ${${REGEN_GENERATED_FOLDERS}} PARENT_SCOPE)
+    set(${ARGS_GENERATED_DIRS} ${${ARGS_GENERATED_DIRS}} PARENT_SCOPE)
 endfunction()
 
 if(NOT REGEN_PATH)
@@ -73,7 +57,7 @@ if(NOT REGEN_PATH)
 endif()
 
 # Find the REGEN_PATH, by looking for the transforms
-find_path(REGEN_PATH ${DATATYPES_XSL} ${COMPONENTS_XSL} HINTS ${REGEN_PATH})
+find_path(REGEN_PATH ${GENERATE_XSL} HINTS ${REGEN_PATH})
 
 # Find the SAXON jar file from the REGEN_PATH
 find_jar(SAXON_JAR name saxon PATHS ${REGEN_PATH})
