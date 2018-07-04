92d4eb049275028bab85d117db0f4c62c60cfe81
JacksonM8 <jackson.m840@gmail.com>
2018-07-04 12:27:31 +0930

    Added logic to only run tests labelled LONG_ in pull requests

--- jenkins/build_re.groovy
+++ jenkins/build_re.groovy
@@ -4,13 +4,19 @@ def PROJECT_NAME = 'test_re'
 import cditma.Utils
 def utils = new Utils(this);
 
+// Override this to enable running of long tests
+def RUN_ALL_TESTS = false
+final branch_name = env.BRANCH_NAME
+if(branch_name.contains("PR-")){
+    RUN_ALL_TESTS = true
+}
+
 node("master"){
     stage("Checkout"){
         dir(PROJECT_NAME){
             checkout scm
             stash includes: "**", name: "source_code"
         }
-        echo sh(returnStdout: true, script: 'env')
     }
 }
 
@@ -82,7 +88,11 @@ for(n in builder_nodes){
                             def file_name = utils.trimExtension(file_path)
                             def test_output = file_name + "_" + node_name + ".xml"
                             print("Running Test: " + file_path)
-                            def test_error_code = utils.runScript("../" + file_path + " --gtest_output=xml:" + test_output)
+                            def test_filter = ""
+                            if (!RUN_ALL_TESTS) {
+                                test_filter = " --gtest_filter=-*LONG_*"
+                            }
+                            def test_error_code = utils.runScript("../" + file_path + " --gtest_output=xml:" + test_output + test_filter)
 
                             if(test_error_code != 0){
                                 FAILED = true
