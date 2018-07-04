6fe61560264aa54bb4201d1a86729727b314c06c
danielrfraser <daniel.raymond.fraser@gmail.com>
2018-07-04 18:07:50 +0930

    Updated ospl to match ;)

--- src/middleware/ospl/pubsub/subscriberport.hpp
+++ src/middleware/ospl/pubsub/subscriberport.hpp
@@ -1,5 +1,5 @@
-#ifndef OSPL_PORTSUBSCRIBER_HPP
-#define OSPL_PORTSUBSCRIBER_HPP
+#ifndef OSPL_PORT_SUBSCRIBER_HPP
+#define OSPL_PORT_SUBSCRIBER_HPP
 
 #include <middleware/ospl/helper.hpp>
 #include <middleware/ospl/pubsub/datareaderlistener.hpp>
@@ -11,9 +11,9 @@
 #include <condition_variable>
 #include <exception>
 
-
 namespace ospl{
-    template <class BaseType, class OsplType> class SubscriberPort: public ::SubscriberPort<BaseType>{
+    template <class BaseType, class OsplType>
+    class SubscriberPort: public ::SubscriberPort<BaseType>{
         friend class DataReaderListener<BaseType, OsplType>;
     public:
         SubscriberPort(std::weak_ptr<Component> component, std::string name, const CallbackWrapper<void, BaseType>& callback_wrapper);
@@ -26,7 +26,7 @@ namespace ospl{
         bool HandlePassivate();
         bool HandleTerminate();
     private:
-        void receive_loop();
+        void recv_loop();
 
         ::Base::Translator<BaseType, OsplType> translator;
 
@@ -38,13 +38,13 @@ namespace ospl{
         std::shared_ptr<Attribute> qos_path_;
         std::shared_ptr<Attribute> qos_name_;
 
-        std::mutex thread_state_mutex_;
-        ThreadState thread_state_;
-        std::condition_variable thread_state_condition_;
+        ThreadManager* thread_manager_ = 0;
+
         
         bool interupt_ = false;
+        int data_available_count = 0;
         std::mutex control_mutex_;
-        std::thread* rec_thread_ = 0;
+        std::thread* recv_thread_ = 0;
         std::mutex notify_mutex_;
         std::condition_variable notify_lock_condition_;
    };
@@ -68,18 +68,14 @@ bool ospl::SubscriberPort<BaseType, OsplType>::HandleConfigure(){
     std::lock_guard<std::mutex> lock(control_mutex_);
     
     bool valid = topic_name_->String().length() >= 0;
-
     if(valid && ::SubscriberPort<BaseType>::HandleConfigure()){
-        if(!rec_thread_){
-            {
-                std::unique_lock<std::mutex> lock(notify_mutex_);
-                interupt_ = false;
-            }
-            std::unique_lock<std::mutex> lock(thread_state_mutex_);
-            thread_state_ = ThreadState::WAITING;
-            rec_thread_ = new std::thread(&ospl::SubscriberPort<BaseType, OsplType>::receive_loop, this);
-            thread_state_condition_.wait(lock, [=]{return thread_state_ != ThreadState::WAITING;});
-            return thread_state_ == ThreadState::STARTED;
+        if(!thread_manager_){
+            thread_manager_ = new ThreadManager();
+            auto future = std::async(std::launch::async, &SubscriberPort<BaseType, OsplType>::recv_loop, this);
+            thread_manager_->SetFuture(std::move(future));
+            return thread_manager_->Configure();
+        }else{
+            std::cerr << "Have extra thread manager" << std::endl;
         }
     }
     return false;
@@ -89,10 +85,9 @@ template <class BaseType, class OsplType>
 bool ospl::SubscriberPort<BaseType, OsplType>::HandlePassivate(){
     std::lock_guard<std::mutex> lock(control_mutex_);
     if(::SubscriberPort<BaseType>::HandlePassivate()){ 
-        //Set the terminate state in the passivation
-        std::lock_guard<std::mutex> lock(notify_mutex_);
-        interupt_ = true;
-        notify();
+        if(thread_manager_){
+            return thread_manager_->Terminate();
+        }
         return true;
     }
     return false;
@@ -103,76 +98,63 @@ bool ospl::SubscriberPort<BaseType, OsplType>::HandleTerminate(){
     HandlePassivate();
     std::lock_guard<std::mutex> lock(control_mutex_);
     if(::SubscriberPort<BaseType>::HandleTerminate()){
-        if(rec_thread_){
-            //Join our ospl_thread
-            rec_thread_->join();
-            delete rec_thread_;
-            rec_thread_ = 0;
+        if(thread_manager_){
+            delete thread_manager_;
+            thread_manager_ = 0;
         }
         return true;
     }
     return false;
 };
 
-template <class BaseType, class OsplType>
-void ospl::SubscriberPort<BaseType, OsplType>::notify(){
-   //Called by the DataReaderListener to notify our SubscriberPort thread to get new data
-   notify_lock_condition_.notify_all();
-};
 
 template <class BaseType, class OsplType>
-void ospl::SubscriberPort<BaseType, OsplType>::receive_loop(){
+void ospl::SubscriberPort<BaseType, OsplType>::recv_loop(){
     dds::sub::DataReader<OsplType> reader_ = dds::sub::DataReader<OsplType>(dds::core::null);
     ospl::DataReaderListener<BaseType, OsplType> listener(this);
 
-    auto state = ThreadState::STARTED;
-
+    bool success = true;
     try{
        //Construct a DDS Paosplcipant, Subscriber, Topic and Reader
        auto helper = DdsHelper::get_dds_helper();    
-       auto participant = helper->get_participant(domain_id_->Integer());
-       auto topic = get_topic<OsplType>(participant, topic_name_->String());
+       auto paosplcipant = helper->get_paosplcipant(domain_id_->Integer());
+       auto topic = get_topic<OsplType>(paosplcipant, topic_name_->String());
 
-       auto subscriber = helper->get_subscriber(participant, subscriber_name_->String());
+       auto subscriber = helper->get_subscriber(paosplcipant, subscriber_name_->String());
        reader_ = get_data_reader<OsplType>(subscriber, topic, qos_path_->String(), qos_name_->String());
-
+       
        //Attach listener to only respond to data_available()
        reader_.listener(&listener, dds::core::status::StatusMask::data_available());
     }catch(const std::exception& ex){
         Log(Severity::ERROR_).Context(this).Func(__func__).Msg(std::string("Unable to startup OSPL DDS Reciever") + ex.what());
-        state = ThreadState::TERMINATED;
+        success = false;
     }
 
-    //Change the state to be Configured
-    {
-        std::lock_guard<std::mutex> lock(thread_state_mutex_);
-        thread_state_ = state;
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-    thread_state_condition_.notify_all();
-
-    if(state == ThreadState::STARTED && Activatable::BlockUntilStateChanged(Activatable::State::RUNNING)){
-        //Log the port becoming online
-        Port::LogActivation();
-        
-        while(true){ 
-            {
-                //Wait for next message
-                std::unique_lock<std::mutex> lock(notify_mutex_);
-                //Check to see if we've been interupted before sleeping the first time
-                if(interupt_){
-                    break;
-                }else{
-                    notify_lock_condition_.wait(lock);
+    if(success){
+        thread_manager_->Thread_Configured();
+        if(thread_manager_->Thread_WaitForActivate()){
+            thread_manager_->Thread_Activated();
+            //Log the port becoming online
+            Port::LogActivation();
+            
+            while(true){ 
+                {
+                    //Wait for next message
+                    std::unique_lock<std::mutex> lock(notify_mutex_);
+                    //Check to see if we've been interupted before sleeping the first time
+                    if(interupt_){
+                        break;
+                    }else{
+                        notify_lock_condition_.wait(lock);
+                    }
                 }
             }
+            Port::LogPassivation();
         }
-        Port::LogPassivation();
         //Blocks for the DataReaderListener to finish
         reader_.close();
     }
+    thread_manager_->Thread_Terminated();
 };
 
-#endif //OSPL_PORTSUBSCRIBER_HPP
+#endif //OSPL_PORT_SUBSCRIBER_HPP
