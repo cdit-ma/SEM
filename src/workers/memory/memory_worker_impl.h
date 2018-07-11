5e45ddab60d63d5d5dd14e2618e87a951df7c87a
JacksonM8 <jackson.m840@gmail.com>
2018-07-11 15:27:26 +0930

    Added mote tests, turned them on, added indeirection of deallocation to avoid recursive_lock

--- src/workers/memory/memory_worker_impl.h
+++ src/workers/memory/memory_worker_impl.h
@@ -12,6 +12,9 @@ class Memory_Worker_Impl{
         size_t GetAllocatedCount();
 
     private:
+
+        bool _NonThreadSafeDeallocate(size_t kilobytes);
+
          /// Type definition for the container of allocations.
         typedef std::list <char *> Memory_Allocations;
 
