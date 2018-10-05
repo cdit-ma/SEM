604f0468c8389718d3a6628c4d233cbf326ab934
danielrfraser <daniel.raymond.fraser@gmail.com>
2018-10-05 12:33:29 +0930

    refactored and updated groovy script for getting Jenkins Nodes to rely on the 're' label to be set for nodes to be scraped MED-422

--- scripts/Jenkins_Construct_GraphMLNodesList.groovy
+++ scripts/Jenkins_Construct_GraphMLNodesList.groovy
@@ -1,38 +1,67 @@
-import jenkins.model.Jenkins;  
-import hudson.slaves.SlaveComputer;  
-import hudson.slaves.DumbSlave;  
+import jenkins.model.Jenkins;
+import hudson.slaves.SlaveComputer;
+import hudson.slaves.DumbSlave;
 import hudson.plugins.sshslaves.SSHLauncher;
 import java.util.regex.*;
 import groovy.json.JsonSlurper;
 
-//Get Jenkins Singleton  
-jenkins.model.Jenkins jenkins = jenkins.model.Jenkins.getInstance();
+//Get Jenkins Singleton
+def jenkins = jenkins.model.Jenkins.getInstance();
+
 ID_COUNTER = 0;
 
-def static Run(nodeName, runCommand)
-{
+def static Run(String node_name, String str_command){
     def output = new java.io.ByteArrayOutputStream();
     def listener = new hudson.util.StreamTaskListener(output);
-    def node = hudson.model.Hudson.instance.getNode(nodeName);
+    def node = hudson.model.Hudson.instance.getNode(node_name);
     def launcher = node.createLauncher(listener);
 
     def command = new hudson.util.ArgumentListBuilder();
-    command.addTokenized(runCommand);
+    command.addTokenized(str_command);
     def ps = launcher.launch();
     ps = ps.cmds(command).stdout(listener);
-    
+
     def proc = launcher.launch(ps);
     int retcode = proc.join();
     return [retcode, output.toString()]
 }
 
+def GetOpenCLDevices(hudson.model.Slave slave){
+    def host_name = slave.getNodeName()
+    def computer = slave.getComputer();
+    def RE_PATH = computer.getEnvironment().get("RE_PATH", "");
 
-def trimWS(String str){
+    if(RE_PATH){
+        try{
+            (recode, output) = Run(host_name, RE_PATH + "/bin/list_opencl_devices");
+            def flat_output = output.replace("\n", "").replace("\r", "");
+            def m = flat_output =~ '(\\{.*\\})'                                           
+            assert m instanceof Matcher                                       
+            if(m){
+                def slurper = new JsonSlurper()
+                return slurper.parseText(m[0][0])
+            }
+        }catch(Exception e){
+
+        }
+    }
+}
+
+def TrimWS(String str){
     return str.trim().replaceAll(" +", " ");
 }
 
+def DoesSlaveHaveLabel(hudson.model.Slave slave, String label){
+    for(tag_cloud_entry in slave.getLabelCloud()){
+        def l = tag_cloud_entry.item.getDisplayName()
+        if(l == label){
+            return true;
+        }
+    }
+    return false;
+}
 
-def getHost(String name) {
+def GetHost(String name) {
     def computer = jenkins.model.Jenkins.getInstance().getComputer(name);
     def node = computer.getNode();
     def label_string = node.getLabelString();
@@ -46,15 +75,56 @@ def getHost(String name) {
 }
 
 def t(int count=0){
-    return "\t" * count;
+    return "    " * count;
 }
 
 def nl(){
     return "\n";
 }
 
-def getID(){
-    return String.valueOf(ID_COUNTER ++);
+def GetID(){
+    return String.valueOf(++ ID_COUNTER);
+}
+
+def GetLastID(){
+    return String.valueOf(ID_COUNTER);
+}
+
+def Key(String key_name, String type, int tab = 0){
+    return t(tab) + '<key attr.name="' + key_name + '" attr.type="' + type + '" for="all" id="' + key_name + '"/>' + nl();
+}
+
+def Graphml(){
+    return '<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns">' + nl();
+}
+
+def EndGraphml(){
+    return '</graphml>' + nl();
+}
+
+def Node(int tab = 0){
+    return t(tab) + '<node id="' + GetID() + '">' + nl();
+}
+
+def EndNode(int tab = 0){
+    return t(tab) + '</node>' + nl();
+}
+
+def EndGraph(int tab = 0){
+    return t(tab) + '</graph>' + nl();
+}
+
+def Graph(int tab = 0, String id = GetLastID()){
+    return t(tab) + '<graph edgedefault="directed" id="' + id + ':">' + nl();
+}
+
+def Data(String key_id, def value, int tab = 0){
+    def str_val = String.valueOf(value)
+    if(str_val != ''){
+        return t(tab) + '<data key="' + key_id + '">' + str_val + '</data>' + nl();
+    }else{
+        return t(tab) + '<data key="' + key_id + '" />' + nl();
+    }
 }
 
 SERVER_NAME = ""
@@ -66,134 +136,123 @@ try{
     SERVER_URL = jenkins.getRootUrl();
     SERVER_NAME = InetAddress.getLocalHost().getHostName();
     USER_NAME = jenkins.getMe().getDisplayName();
-}catch(Exception e){
-}
-ID_COUNTER = 0
+}catch(Exception e){}
 
-OUTPUT = '<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns">' << nl();
-OUTPUT <<= '<key attr.name="kind" attr.type="string" for="all" id="k1"/>' << nl();
-OUTPUT <<= '<key attr.name="type" attr.type="string" for="all" id="k2"/>' << nl();
-OUTPUT <<= '<key attr.name="label" attr.type="string" for="all" id="k3"/>' << nl();
-OUTPUT <<= '<key attr.name="description" attr.type="string" for="all" id="k4"/>' << nl();
-OUTPUT <<= '<key attr.name="url" attr.type="string" for="all" id="k5"/>' << nl();
-OUTPUT <<= '<key attr.name="uuid" attr.type="string" for="all" id="k23"/>' << nl();
-OUTPUT <<= '<key attr.name="ip_address" attr.type="string" for="all" id="k6"/>' << nl();
-OUTPUT <<= '<key attr.name="os" attr.type="string" for="all" id="k7"/>' << nl();
-OUTPUT <<= '<key attr.name="architecture" attr.type="string" for="all" id="k8"/>' << nl();
-OUTPUT <<= '<key attr.name="os_version" attr.type="string" for="all" id="k9"/>' << nl();
-OUTPUT <<= '<key attr.name="readOnly" attr.type="boolean" for="all" id="k11"/>' << nl();
-OUTPUT <<= '<key attr.name="index" attr.type="int" for="all" id="k12"/>' << nl();
-OUTPUT <<= '<key attr.name="root_path" attr.type="string" for="all" id="k13"/>' << nl();
-OUTPUT <<= '<key attr.name="load_time" attr.type="string" for="all" id="k14"/>' << nl();
-OUTPUT <<= '<key attr.name="is_online" attr.type="boolean" for="all" id="k15"/>' << nl();
-OUTPUT <<= '<key attr.name="user_name" attr.type="string" for="all" id="k16"/>' << nl();
-OUTPUT <<= '<key attr.name="version" attr.type="string" for="all" id="version"/>' << nl();
-OUTPUT <<= '<key attr.name="isExpanded" attr.type="boolean" for="all" id="k22"/>' << nl();
-OUTPUT <<= '<key attr.name="vendor" attr.type="string" for="all" id="vendor"/>' << nl();
-OUTPUT <<= '<key attr.name="is_available" attr.type="boolean" for="all" id="is_available"/>' << nl();
-OUTPUT <<= '<key attr.name="memory_size" attr.type="int" for="all" id="memory_size"/>' << nl();
-OUTPUT <<= '<key attr.name="clock_freq" attr.type="int" for="all" id="clock_freq"/>' << nl();
+//Export Keys
+OUTPUT = Graphml()
+OUTPUT += Key("kind", "string");
+OUTPUT += Key("type", "string");
+OUTPUT += Key("label", "string");
+OUTPUT += Key("description", "string");
+OUTPUT += Key("url", "string");
+OUTPUT += Key("uuid", "string");
+OUTPUT += Key("ip_address", "string");
+OUTPUT += Key("os", "string");
+OUTPUT += Key("architecture", "string");
+OUTPUT += Key("os_version", "string");
+OUTPUT += Key("readOnly", "boolean");
+OUTPUT += Key("index", "int");
+OUTPUT += Key("root_path", "string");
+OUTPUT += Key("load_time", "string");
+OUTPUT += Key("is_online", "boolean");
+OUTPUT += Key("user_name", "string");
+OUTPUT += Key("version", "string");
+OUTPUT += Key("isExpanded", "boolean");
+OUTPUT += Key("is_available", "boolean");
+OUTPUT += Key("vendor", "string");
+OUTPUT += Key("memory_size", "long");
+OUTPUT += Key("clock_freq", "long");
 
+OUTPUT += Graph();
+OUTPUT += Node(1);
+OUTPUT += Data('kind', 'HardwareDefinitions', 2);
+OUTPUT += Graph(2);
+OUTPUT += Node(3);
+OUTPUT += Data('kind', 'HardwareCluster', 4);
+OUTPUT += Data('label', SERVER_NAME, 4);
+OUTPUT += Data('url', SERVER_URL, 4);
+OUTPUT += Data('uuid', SERVER_URL, 4);
+OUTPUT += Data('user_name', USER_NAME, 4);
+OUTPUT += Data('load_time', LOAD_TIME, 4);
+OUTPUT += Data('isExpanded', true, 4);
+OUTPUT += Data('readOnly', true, 4);
+OUTPUT += Data('is_online', true, 4);
+OUTPUT += Graph(4);
 
-OUTPUT <<= '<graph edgedefault="directed" id="' << getID() << '">' << nl();
-OUTPUT <<= t(1) << '<node id="' << getID() << '">' << nl();
-OUTPUT <<= t(2) << '<data key="k1">HardwareDefinitions</data>' << nl();
-OUTPUT <<= t(2) << '<graph edgedefault="directed" id="' << getID() << '">' << nl();
-OUTPUT <<= t(3) << '<node id="' << getID() << '">' << nl();
-
-//Setup Cluster information
-OUTPUT <<= t(3) << '<data key="k1">HardwareCluster</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k3">' << SERVER_NAME << '</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k5">' << SERVER_URL << '</data>' << nl();
-OUTPUT <<= t(6) << '<data key="k23">' << SERVER_URL << '</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k16">' << USER_NAME << '</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k14">' << LOAD_TIME << '</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k22">true</data>' << nl();
-OUTPUT <<= t(4) << '<data key="k11">true</data>' << nl();
-OUTPUT <<= t(4) << '<graph edgedefault="directed" id="' << getID() << '">' << nl();
-
-sort_order = 0
-//Deal with the nodes
+def index = 0
 for(slave in jenkins.slaves){
-    hudson.model.Computer c = slave.getComputer();
-    online = c.isOffline() ? "false" : "true";
-    hostname = slave.getNodeName();
-    IP = getHost(hostname);
-
-    OUTPUT <<= t(5) << '<node id="' << getID() << '">' << nl();
-    OUTPUT <<= t(6) << '<data key="k1">HardwareNode</data>' << nl();  
-    OUTPUT <<= t(6) << '<data key="k22">true</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k3">' << slave.getNodeName() << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k4">' << slave.getLabelString() << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k6">' << IP << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k5">' << SERVER_URL << c.getUrl() << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k23">' << SERVER_URL << c.getUrl() << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k15">' << online << '</data>' << nl();
-    OUTPUT <<= t(6) << '<data key="k11">true</data>' << nl();
-
-    def RE_PATH = c.getEnvironment().get("RE_PATH", "");
-
-    if(!c.isOffline()){
-        //Can only get whilst online
-        OUTPUT <<= t(6) << '<data key="k7">' << c.getSystemProperties().get("os.name", "") << '</data>' << nl();
-        OUTPUT <<= t(6) << '<data key="k8">' << c.getSystemProperties().get("os.arch", "") << '</data>' << nl();
-        OUTPUT <<= t(6) << '<data key="k9">' << c.getSystemProperties().get("os.version", "") << '</data>' << nl();
-        OUTPUT <<= t(6) << '<data key="k13">' << slave.getRootPath() << '</data>' << nl();
-    
-        if(RE_PATH != ""){
-            try{
-                (recode, output) = Run(hostname, RE_PATH + "/bin/list_opencl_devices");
-                def flat_output = output.replace("\n", "").replace("\r", "");
-                def m = flat_output =~ '(\\{.*\\})'                                           
-                assert m instanceof Matcher                                       
-                    if (!m) {                                                         
-                        throw new RuntimeException("Oops, text not found!")
-                }else{
-                    def slurper = new JsonSlurper()
-                    def result = slurper.parseText(m[0][0])
-        
-                    OUTPUT <<= t(6) << '<graph edgedefault="directed" id="' << getID() << '">' << nl();
-        
-                    for(platform in result.platforms){
-                        OUTPUT <<= t(7) << '<node id="' << getID() << '">' << nl();
-                        OUTPUT <<= t(8) << '<data key="k1">OpenCLPlatform</data>' << nl();  
-                        OUTPUT <<= t(8) << '<data key="k3">' << trimWS(platform.name) << '</data>' << nl();  
-                        OUTPUT <<= t(8) << '<data key="version">' << platform.version << '</data>' << nl();
-                        OUTPUT <<= t(8) << '<data key="vendor">' << platform.vendor << '</data>' << nl();
-        
-                        OUTPUT <<= t(8) << '<graph edgedefault="directed" id="' << getID() << '">' << nl();
-                        for(device in platform.devices){
-                            OUTPUT <<= t(9) << '<node id="' << getID() << '">' << nl();
-                            OUTPUT <<= t(10) << '<data key="k1">OpenCLDevice</data>' << nl();  
-                            OUTPUT <<= t(10) << '<data key="k3">' << trimWS(device.name) << '</data>' << nl();  
-                            OUTPUT <<= t(10) << '<data key="is_available">' << device.available << '</data>' << nl();
-                            OUTPUT <<= t(10) << '<data key="memory_size">' << device.mem_size << '</data>' << nl();
-                            OUTPUT <<= t(10) << '<data key="clock_freq">' << device.max_clock_frequency << '</data>' << nl();
-                            OUTPUT <<= t(10) << '<data key="k2">' << device.type << '</data>' << nl();
-                            OUTPUT <<= t(10) << '<data key="version">' << device.driver_version << '</data>' << nl();
-                            OUTPUT <<= t(9) << '</node>' << nl();
-                        }
-                        OUTPUT <<= t(9) << '</graph>' << nl();
-                        OUTPUT <<= t(8) << '</node>' << nl();
-                    }
-                    OUTPUT <<= t(7) << '</graph>' << nl();
-                }
-            }catch(Exception e) {
-            
-            } 
-        }
-        OUTPUT <<= t(6) << '<data key="k12">' << sort_order++ << '</data>' << nl();
-        OUTPUT <<= t(6) << '<data key="k14">' << LOAD_TIME << '</data>' << nl();
+    //Ignore non-re slaves
+    if(!DoesSlaveHaveLabel(slave, "re")){
+        continue;
     }
-    OUTPUT <<= t(5) << '</node>' << nl();
+
+    def host_name = slave.getNodeName()
+    def computer = slave.getComputer();
+    def system_properties = computer.getSystemProperties();
+    def is_online = !computer.isOffline()
+    def node_uuid = SERVER_URL + computer.getUrl()
+
+    OUTPUT += Node(5);
+    OUTPUT += Data('kind', 'HardwareNode', 6);
+    OUTPUT += Data('label', host_name, 6);
+    OUTPUT += Data('isExpanded', true, 6);
+    OUTPUT += Data('description', slave.getLabelString(), 6);
+    OUTPUT += Data('ip_address', GetHost(host_name), 6);
+    OUTPUT += Data('url', SERVER_URL + computer.getUrl(), 6);
+    OUTPUT += Data('uuid', node_uuid, 6);
+    OUTPUT += Data('is_online', is_online, 6);
+    OUTPUT += Data('readOnly', true, 6);
+    OUTPUT += Data('os', system_properties.get("os.name", ""), 6);
+    OUTPUT += Data('architecture', system_properties.get("os.arch", ""), 6);
+    OUTPUT += Data('os_version', system_properties.get("os.version", ""), 6);
+    OUTPUT += Data('root_path', slave.getRootPath(), 6);
+    OUTPUT += Data('index', index++, 6);
+    OUTPUT += Data('load_time', LOAD_TIME, 6);
+
+    if(is_online){
+        def opencl_json = GetOpenCLDevices(slave)
+        if(opencl_json){
+            OUTPUT += Graph(6);
+
+            for(platform in opencl_json.platforms){
+                def platform_name = TrimWS(platform.name)
+                def platform_uuid = node_uuid + "/" + platform_name
+                OUTPUT += Node(7);
+                OUTPUT += Data('kind', 'OpenCLPlatform', 8);
+                OUTPUT += Data('label', platform_name, 8);
+                OUTPUT += Data('version', platform.version, 8);
+                OUTPUT += Data('vendor', platform.vendor, 8);
+                OUTPUT += Data('uuid', platform_uuid, 8);
+                OUTPUT += Graph(8);
+
+                for(device in platform.devices){
+                    def device_name = TrimWS(device.name)
+                    def device_uuid = platform_uuid + "/" + device_name
+                    OUTPUT += Node(9);
+                    OUTPUT += Data('kind', 'OpenCLDevice', 10);
+                    OUTPUT += Data('label', TrimWS(device.name), 10);
+                    OUTPUT += Data('is_available', device.available, 10);
+                    OUTPUT += Data('memory_size', device.mem_size, 10);
+                    OUTPUT += Data('clock_freq', device.max_clock_frequency, 10);
+                    OUTPUT += Data('type', device.type, 10);
+                    OUTPUT += Data('uuid', device_uuid, 10);
+                    OUTPUT += Data('vendor', device.driver_version, 10);
+                    OUTPUT += EndNode(9);
+                }
+
+                OUTPUT += EndGraph(8);
+                OUTPUT += EndNode(7);
+            }
+            OUTPUT += EndGraph(6);
+        }
+    }
+    OUTPUT += EndNode(5);
 }
 
-//Finish off document
-OUTPUT <<= t(4) << '</graph>' << nl();
-OUTPUT <<= t(3) << '</node>' << nl();
-OUTPUT <<= t(2) << '</graph>' << nl();
-OUTPUT <<= t(1) << '</node>' << nl();
-OUTPUT <<= '</graph>' << nl();
-OUTPUT <<= '</graphml>' << nl();
-
+OUTPUT += EndGraph(4);
+OUTPUT += EndNode(3);
+OUTPUT += EndGraph(2);
+OUTPUT += EndNode(1);
+OUTPUT += EndGraph();
+OUTPUT += EndGraphml();
 println(OUTPUT);
