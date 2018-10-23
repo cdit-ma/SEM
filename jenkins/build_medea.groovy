95b4788494379f3b83675863fadf5d4f12d7cee8
danielrfraser <daniel.raymond.fraser@gmail.com>
2018-10-23 11:38:03 +1030

    Testing git auth

--- jenkins/build_medea.groovy
+++ jenkins/build_medea.groovy
@@ -86,4 +86,30 @@ if(BUILD_PACKAGE){
     stage("Package"){
         parallel step_archive
     }
+
+    stage("Release"){
+        node("master"){
+            withCredentials([usernamePassword(credentialsId: 'git-auth-token', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                print('GIT USER:${GIT_USERNAME}')
+            }
+        }
+    }
 }
+/*
+
+    echo "Compressing artifacts into one file"
+zip -r artifacts.zip artifacts_folder
+
+echo "Exporting token and enterprise api to enable github-release tool"
+export GITHUB_TOKEN=$$$$$$$$$$$$
+export GITHUB_API=https://git.{your domain}.com/api/v3 # needed only for enterprise
+
+echo "Deleting release from github before creating new one"
+github-release delete --user ${GITHUB_ORGANIZATION} --repo ${GITHUB_REPO} --tag ${VERSION_NAME}
+
+echo "Creating a new release in github"
+github-release release --user ${GITHUB_ORGANIZATION} --repo ${GITHUB_REPO} --tag ${VERSION_NAME} --name "${VERSION_NAME}"
+
+echo "Uploading the artifacts into github"
+github-release upload --user ${GITHUB_ORGANIZATION} --repo ${GITHUB_REPO} --tag ${VERSION_NAME} --name "${PROJECT_NAME}-${VERSION_NAME}.zip" --file artifacts.zip
+}*/
