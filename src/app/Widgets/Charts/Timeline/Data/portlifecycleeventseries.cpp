69edd9716918a023d4779422690ad7afd9a3304c
cathlynaston <cm.miller@y7mail.com>
2018-11-13 15:32:51 +1030

    Added renamed files

---
+++ src/app/Widgets/Charts/Timeline/Data/portlifecycleeventseries.cpp
@@ -0,0 +1,63 @@
+#include "portlifecycleseries.h"
+
+
+int PortLifeCycleSeries::series_ID = 0;
+
+/**
+ * @brief PortLifeCycleSeries::PortLifeCycleSeries
+ * @param parent
+ */
+PortLifeCycleSeries::PortLifeCycleSeries(QObject* parent)
+    : BaseSeries(parent)
+{
+    ID_ = series_ID++;
+}
+
+/*
+PortLifeCycleSeries::PortLifeCycleSeries(int ID, QObject *parent)
+    : QObject(parent)
+{
+    ID_ = ID;
+}
+*/
+
+
+/**
+ * @brief PortLifeCycleSeries::getID
+ * @return
+ */
+int PortLifeCycleSeries::getID()
+{
+    return -1; //ID_;
+}
+
+
+/**
+ * @brief PortLifeCycleSeries::addPortEvent
+ * @param event
+ */
+void PortLifeCycleSeries::addPortEvent(PortLifeCycle* event)
+{
+    portEvents_.append(event);
+}
+
+
+/**
+ * @brief PortLifeCycleSeries::addPortEvents
+ * @param events
+ */
+void PortLifeCycleSeries::addPortEvents(QList<PortLifeCycle*> events)
+{
+    portEvents_.append(events);
+}
+
+
+/**
+ * @brief PortLifeCycleSeries::getConstPortEvents
+ * @return
+ */
+const QList<PortLifeCycle*> &PortLifeCycleSeries::getConstPortEvents()
+{
+    return portEvents_;
+}
+
