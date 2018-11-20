77f507517db7ffadc16c45366bb28cf7b0a353f3
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-11-20 11:53:12 +1030

    Add build docker images script.

---
+++ docker/build_docker_images.groovy
@@ -0,0 +1,9 @@
+
+
+node("docker") {
+    checkout scm
+    def minimal_deps = docker.build("minimal_deps", "re/docker/minimal_deps")
+    def full_deps = docker.build("full_deps", "re/docker/full_deps")
+    def re_minimal = docker.build("re_minimal", "re/docker/re_minimal")
+    def re_full = docker.build("re_full", "re/docker/re_full")
+}
