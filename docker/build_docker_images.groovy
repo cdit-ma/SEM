400003eed1827053df7976dfa5791ae5c583347b
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-11-20 12:31:29 +1030

    Update build docker images script.

--- docker/build_docker_images.groovy
+++ docker/build_docker_images.groovy
@@ -2,8 +2,9 @@
 
 node("docker_runtime") {
     checkout scm
-    def minimal_deps = docker.build("minimal_deps", "docker/minimal_deps")
-    def full_deps = docker.build("full_deps", "docker/full_deps")
-    def re_minimal = docker.build("re_minimal", "docker/re_minimal")
-    def re_full = docker.build("re_full", "docker/re_full")
+    
+    def minimal_deps = docker.build("minimal_deps", "-f docker/minimal_deps/Dockerfile")
+    def full_deps = docker.build("full_deps", "-f docker/full_deps/Dockerfile")
+    def re_minimal = docker.build("re_minimal", "-f docker/re_minimal/Dockerfile")
+    def re_full = docker.build("re_full", "-f docker/re_full/Dockerfile")
 }
