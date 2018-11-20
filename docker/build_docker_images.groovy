23a573ec9dcdf30abbbfce2bec1cd348bb95c070
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-11-20 13:47:46 +1030

    Update docker image build script.

--- docker/build_docker_images.groovy
+++ docker/build_docker_images.groovy
@@ -1,10 +1,17 @@
 
+// Override this to enable running of long tests
+final Boolean IS_TAG = env.TAG_NAME
+final GIT_ID = IS_TAG ? env.TAG_NAME : env.BRANCH_NAME
 
 node("docker_runtime") {
     checkout scm
     
-    def minimal_deps = docker.build("minimal_deps", "-f docker/minimal_deps/Dockerfile .")
-    def full_deps = docker.build("full_deps", "-f docker/full_deps/Dockerfile .")
-    def re_minimal = docker.build("re_minimal", "-f docker/re_minimal/Dockerfile .")
-    def re_full = docker.build("re_full", "-f docker/re_full/Dockerfile .")
+    def minimal_deps = docker.build("minimal_deps:${GIT_ID}", "-f docker/minimal_deps/Dockerfile .")
+    minimal_deps.push()
+    def full_deps = docker.build("full_deps:${GIT_ID}", "-f docker/full_deps/Dockerfile .")
+    full_deps.push()
+    def re_minimal = docker.build("re_minimal:${GIT_ID}", "-f docker/re_minimal/Dockerfile .")
+    re_minimal.push()
+    def re_full = docker.build("re_full:${GIT_ID}", "-f docker/re_full/Dockerfile .")
+    re_full.push()
 }
