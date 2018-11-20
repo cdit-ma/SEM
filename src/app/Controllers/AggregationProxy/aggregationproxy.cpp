c06acc7abcba6da2636696fc757ea0100e75ec8d
cathlynaston <cm.miller@y7mail.com>
2018-11-20 12:32:51 +1030

    Fixed linking error

--- src/app/Controllers/AggregationProxy/aggregationproxy.cpp
+++ src/app/Controllers/AggregationProxy/aggregationproxy.cpp
@@ -1,29 +1,110 @@
 #include "aggregationproxy.h"
-#include <iostream>
 #include "../../Controllers/NotificationManager/notificationmanager.h"
 #include "../../Controllers/NotificationManager/notificationobject.h"
 
-AggregationProxy::AggregationProxy():
-controller_("tcp://192.168.111.187:20000")
+#include <iostream>
+#include <QtConcurrent>
+
+
+AggregationProxy::AggregationProxy() :
+    requester_("tcp://192.168.111.249:12345")
 {
 }
 
 
-void AggregationProxy::RequestRunningExperiments(){
-    auto notification = NotificationManager::manager()->AddNotification("Requesting Running Experiments", "Icons", "buildingPillared", Notification::Severity::RUNNING, Notification::Type::APPLICATION, Notification::Category::NONE);
-    
-
-
-    try{
-        auto results = controller_.ListExperiments();
-        std::cerr << "* Got Experiments" << std::endl;
-        for(auto result : results){
-            NotificationManager::manager()->AddNotification(QString("Got Experiment: ") + QString(result.c_str()), "Icons", "circleBoltDark", Notification::Severity::INFO, Notification::Type::APPLICATION, Notification::Category::NONE);
-        }
+void AggregationProxy::RequestRunningExperiments()
+{
+    /*auto notification = NotificationManager::manager()->AddNotification("Requesting Port Lifecycle", "Icons", "buildingPillared", Notification::Severity::RUNNING, Notification::Type::APPLICATION, Notification::Category::NONE);
+    try {
+        AggServer::PortLifecycleRequest request;
+        auto results = requester_.GetPortLifecycle(request);
+        qDebug() << "Request result size#: " << results.get()->events_size();
         notification->setSeverity(Notification::Severity::SUCCESS);
-    }catch(const std::exception& ex){
+
+    } catch (const std::exception& ex) {
+        notification->setSeverity(Notification::Severity::ERROR);
+        notification->setDescription(ex.what());
+    }*/
+
+    AggServer::PortLifecycleRequest request;
+    SendPortLifecycleRequest(request);
+}
+
+
+/**
+ * @brief AggregationProxy::RequestRunningExperiments
+ * @param fromTimeMS
+ * @param toTimeMS
+ */
+void AggregationProxy::RequestRunningExperiments(qint64 fromTimeMS, qint64 toTimeMS)
+{
+    AggServer::PortLifecycleRequest request;
+    request.mutable_time_interval()->AddAllocated(constructTimestampFromMS(fromTimeMS).release());
+    request.mutable_time_interval()->AddAllocated(constructTimestampFromMS(toTimeMS).release());
+    SendPortLifecycleRequest(request);
+}
+
+
+/**
+ * @brief AggregationProxy::constructTimestampFromMS
+ * @param milliseconds
+ * @return
+ */
+std::unique_ptr<google::protobuf::Timestamp> AggregationProxy::constructTimestampFromMS(qint64 milliseconds)
+{
+    google::protobuf::Timestamp timestamp = google::protobuf::util::TimeUtil::MillisecondsToTimestamp(milliseconds);
+    return std::unique_ptr<google::protobuf::Timestamp>(new google::protobuf::Timestamp(timestamp));
+}
+
+
+/**
+ * @brief AggregationProxy::getQDateTime
+ * @param time
+ * @return
+ */
+const QDateTime AggregationProxy::getQDateTime(const google::protobuf::Timestamp &time)
+{
+    int64_t mu = google::protobuf::util::TimeUtil::TimestampToMicroseconds(time);
+    return QDateTime::fromMSecsSinceEpoch(mu / 1E3, Qt::UTC);
+}
+
+
+/**
+ * @brief AggregationProxy::getQString
+ * @param string
+ * @return
+ */
+const QString AggregationProxy::getQString(const std::string &string)
+{
+    return QString::fromUtf8(string.c_str());
+}
+
+
+/**
+ * @brief AggregationProxy::SendPortLifecycleRequest
+ * @param request
+ */
+void AggregationProxy::SendPortLifecycleRequest(AggServer::PortLifecycleRequest &request)
+{
+    auto notification = NotificationManager::manager()->AddNotification("Requesting Port Lifecycle", "Icons", "buildingPillared", Notification::Severity::RUNNING, Notification::Type::APPLICATION, Notification::Category::NONE);
+
+    try {
+        auto start = QDateTime::currentMSecsSinceEpoch();
+        auto results = requester_.GetPortLifecycle(request);
+
+        qDebug() << "--------------------------------------------------------------------------------";
+        qDebug() << "Request result size#: " << results.get()->events_size();
+
+        // TODO - Do work here!
+
+        auto finish = QDateTime::currentMSecsSinceEpoch();
+        qDebug() << "Construction of portlifecycle widgets and series WITH clearing took: " << finish - start << " ms.";
+        qDebug() << "--------------------------------------------------------------------------------";
+
+        notification->setSeverity(Notification::Severity::SUCCESS);
+
+    } catch (const std::exception& ex) {
         notification->setSeverity(Notification::Severity::ERROR);
         notification->setDescription(ex.what());
     }
-    
 }
--- src/app/Controllers/AggregationProxy/aggregationproxy.h
+++ src/app/Controllers/AggregationProxy/aggregationproxy.h
@@ -1,19 +1,29 @@
-
 #ifndef MEDEA_AGGREGATIONPROXY_H
 #define MEDEA_AGGREGATIONPROXY_H
 
 #include <QObject>
-#include <comms/environmentcontroller/environmentcontroller.h>
+
+#include <google/protobuf/util/time_util.h>
+#include <comms/aggregationrequester/aggregationrequester.h>
 
 class AggregationProxy : public QObject
 {
     Q_OBJECT
+
 public:
     AggregationProxy();
 
     void RequestRunningExperiments();
+    void RequestRunningExperiments(qint64 fromTimeMS, qint64 toTimeMS);
+
+    static std::unique_ptr<google::protobuf::Timestamp> constructTimestampFromMS(qint64 milliseconds);
+    static const QDateTime getQDateTime(const google::protobuf::Timestamp &time);
+    static const QString getQString(const std::string &string);
+
 private:
-    EnvironmentManager::EnvironmentController controller_;
+    void SendPortLifecycleRequest(AggServer::PortLifecycleRequest& request);
+    AggServer::Requester requester_;
+
 };
 
 #endif // MEDEA_AGGREGATIONPROXY_H
