c3f34200bb173bbac2a5fa378b9bc8071dc85270
mitchell-conrad <mitchell.w.conrad@gmail.com>
2018-11-20 13:49:42 +1030

    Update docker image build script.

--- docker/build_docker_images.groovy
+++ docker/build_docker_images.groovy
@@ -6,12 +6,14 @@ final GIT_ID = IS_TAG ? env.TAG_NAME : env.BRANCH_NAME
 node("docker_runtime") {
     checkout scm
     
-    def minimal_deps = docker.build("minimal_deps:${GIT_ID}", "-f docker/minimal_deps/Dockerfile .")
-    minimal_deps.push()
-    def full_deps = docker.build("full_deps:${GIT_ID}", "-f docker/full_deps/Dockerfile .")
-    full_deps.push()
-    def re_minimal = docker.build("re_minimal:${GIT_ID}", "-f docker/re_minimal/Dockerfile .")
-    re_minimal.push()
-    def re_full = docker.build("re_full:${GIT_ID}", "-f docker/re_full/Dockerfile .")
-    re_full.push()
+    docker.withRegistry('192.168.111.98:5000'){
+        def minimal_deps = docker.build("minimal_deps:${GIT_ID}", "-f docker/minimal_deps/Dockerfile .")
+        minimal_deps.push()
+        def full_deps = docker.build("full_deps:${GIT_ID}", "-f docker/full_deps/Dockerfile .")
+        full_deps.push()
+        def re_minimal = docker.build("re_minimal:${GIT_ID}", "-f docker/re_minimal/Dockerfile .")
+        re_minimal.push()
+        def re_full = docker.build("re_full:${GIT_ID}", "-f docker/re_full/Dockerfile .")
+        re_full.push()
+    }
 }
