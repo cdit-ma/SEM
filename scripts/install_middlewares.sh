5b76f379456b619004af8b456fb841472f093e81
mitchell-conrad <mitchell.w.conrad@gmail.com>
2019-01-14 16:51:33 +1030

    Add configure script, update install scripts.

--- scripts/install_middlewares.sh
+++ scripts/install_middlewares.sh
@@ -1,11 +1,26 @@
-
+#Build Deps
+#UBUNTU
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y expect sed python-minimal uuid-dev ruby bison
 
+#Build Deps
+#CENTOS
+sudo yum install libuuid-devel bison sed ruby
+
+#RTI
+#UBUNTU
 wget http://192.168.111.1/raid/software_share/Middlewares/DDS/RTI/5.3/rti_connext_dds-5.3.0-eval-x64Linux3gcc5.4.0.run -q && \
     chmod +x rti_connext_dds-5.3.0-eval-x64Linux3gcc5.4.0.run && \
     sudo ./rti_connext_dds-5.3.0-eval-x64Linux3gcc5.4.0.run && \
     rm -rf rti*
 
+#RTI
+#CENTOS
+wget http://192.168.111.1/raid/software_share/Middlewares/DDS/RTI/5.3/rti_connext_dds-5.3.0-eval-x64Linux3gcc4.8.2.run -q && \
+    chmod +x rti_connext_dds-5.3.0-eval-x64Linux3gcc4.8.2.run && \
+    sudo ./rti_connext_dds-5.3.0-eval-x64Linux3gcc4.8.2.run && \
+    rm -rf rti*
+
+#OpenSplice
 sudo wget http://192.168.111.1/raid/software_share/Middlewares/DDS/OpenSpliceDDS/6.4/OpenSpliceDDSV6.4.140407OSS-HDE-x86_64.linux-gcc4.6-glibc2.15-installer.tar.gz -q -P /opt/ && \
     sudo tar xf /opt/OpenSpliceDDSV6.4.140407OSS-HDE-x86_64.linux-gcc4.6-glibc2.15-installer.tar.gz -C /opt/ && \
     sudo cp Makefile.Build_DCPS_ISO_Cpp_Lib /opt/HDE/x86_64.linux/custom_lib/Makefile.Build_DCPS_ISO_Cpp_Lib && \
@@ -15,6 +30,7 @@ sudo wget http://192.168.111.1/raid/software_share/Middlewares/DDS/OpenSpliceDDS
     sudo make Build_DCPS_ISO_Cpp_Lib && \
     sudo rm -rf /opt/OpenSplice*
 
+#QpidPb
 wget http://192.168.111.1/raid/software_share/Middlewares/QpidPb/qpid-cpp-1.38.0.tar.gz -q && \
     wget http://192.168.111.1/raid/software_share/Middlewares/QpidPb/qpid-proton-0.24.0.tar.gz -q && \
     sudo tar xf qpid-proton-0.24.0.tar.gz -C /opt/ && \
@@ -28,5 +44,7 @@ wget http://192.168.111.1/raid/software_share/Middlewares/QpidPb/qpid-cpp-1.38.0
     cd /opt/qpid-cpp-1.38.0/build && \
     sudo cmake -G Ninja .. -DCMAKE_INSTALL_PREFIX=/opt/QpidPb -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python -DBUILD_BINDING_PERL=OFF -DBUILD_BINDING_PYTHON=OFF -DBUILD_BINDING_RUBY=OFF && \
     sudo cmake --build . --target install && \
+    cd /opt && \
+    sudo rm -rf qpid*
     cd ~ && \
     sudo rm -rf qpid*
