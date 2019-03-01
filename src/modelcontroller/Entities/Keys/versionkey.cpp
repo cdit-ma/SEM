523c3656f46513cf120e2c078911701cca99985e
danielrfraser <daniel.raymond.fraser@gmail.com>
2019-03-01 14:25:17 +1030

    Updated to use QRegularExpression instead of QRegExp MED-538

--- src/modelcontroller/Entities/Keys/versionkey.cpp
+++ src/modelcontroller/Entities/Keys/versionkey.cpp
@@ -2,8 +2,9 @@
 #include "../data.h"
 #include "../node.h"
 #include <QDebug>
+#include <QRegularExpression>
 
-const QString regex_str{R"(^(\d+)\.(\d+)\.(\d+)(-.+)?$)"};
+const QRegularExpression VERSION_REGEX{R"(^(\d+)\.(\d+)\.(\d+)(-.+)?$)"};
 
 
 VersionKey::VersionKey(EntityFactoryBroker& broker): Key(broker, KeyName::Version, QVariant::String)
@@ -25,25 +26,22 @@ QVariant VersionKey::validateDataChange(Data* data, QVariant data_value){
 // eg 0.1.16-test
 // See: https://semver.org/ for full specification (we allow an arbitrary string on the end)
 int VersionKey::CompareVersion(const QString& current_version, const QString& compare_version){
-    QRegExp current_rx(regex_str);
-    QRegExp compare_rx(regex_str);
-
-    auto current_valid = current_rx.exactMatch(current_version);
-    auto compare_valid = compare_rx.exactMatch(compare_version);
-
-    if(!current_valid) {
+    const auto current_match = VERSION_REGEX.match(current_version);
+    const auto compare_match = VERSION_REGEX.match(compare_version);
+    
+    if(!current_match.hasMatch()) {
         throw std::invalid_argument("Current Version invalid format");
     }
     
-    if(!compare_valid) {
+    if(!compare_match.hasMatch()) {
         throw std::invalid_argument("Compare Version invalid format");
     }
 
     // Iterate over the three numeric fields
     // First matching regex group is start caret, therefore skip and start at group 1
     for(int i = 1; i < 4; i++) {
-        int current = current_rx.cap(i).toInt();
-        int compare = compare_rx.cap(i).toInt();
+        int current = current_match.captured(i).toInt();
+        int compare = compare_match.captured(i).toInt();
 
         if(current == compare) {
             // Keep comparing if we have a match
@@ -57,12 +55,11 @@ int VersionKey::CompareVersion(const QString& current_version, const QString& co
     }
 
     // All numeric version numbers match, compare suffix strings lexicographically
-    return current_rx.cap(4).compare(compare_rx.cap(4), Qt::CaseInsensitive);
+    return current_match.captured(4).compare(compare_match.captured(4), Qt::CaseInsensitive);
 }
 
 bool VersionKey::IsVersionValid(const QString& version){
-    QRegExp current_regex(regex_str);
-    return current_regex.exactMatch(version);
+    return VERSION_REGEX.match(version).hasMatch();
 }
 
 const QString& VersionKey::GetDefaultVersion(){
