4bdaef2844bc23ad60713cae59f20e0ec6510046
Dan Fraser <daniel.raymond.fraser@gmail.com>
2019-05-10 10:52:18 +0930

    Regen 96 (#123)
    * Updated re_gen unit tests to rely on a python script run_tests.py REGEN-96
    
    * Fixed issue with centos and python
    
    * Fixed issue with argv REGEN-96
    
    * Removed spurius imports REGEN-96
    
    * Fixed syntax issue with Junit file

--- jenkins/build_re.groovy
+++ jenkins/build_re.groovy
@@ -1,7 +1,5 @@
 #!groovy
 @Library('cditma-utils') _
-import groovy.time.TimeCategory 
-import groovy.time.TimeDuration
 import hudson.tasks.test.AbstractTestResultAction
 def utils = new cditma.Utils(this)
 
@@ -10,7 +8,6 @@ final GIT_ID = IS_TAG ? env.TAG_NAME : env.BRANCH_NAME
 //Run full tests on a Tag or a Pull Request
 final RUN_ALL_TESTS = IS_TAG || GIT_ID.contains("PR-") || GIT_ID.contains("release-")
 def RELEASE_DESCRIPTION = "re-" + GIT_ID
-
 
 @NonCPS
 def get_test_status(){
@@ -140,63 +137,17 @@ pipeline{
                                     }
                                 }
                                 if(RUN_ALL_TESTS){
-                                    dir("test/re_gen/models"){
-                                        def glob_str = "*.graphml"
-                                        def graphml_list = findFiles glob: glob_str
+                                    dir("test/re_gen"){
+                                        def test_suite_xml = "${node_name}_regen_tests.xml"
 
-                                        def test_count = 0;
-                                        def fail_count = 0;
-                                        def test_time = 0;
-
-                                        def test_suites = ""
-                                        def test_suite_name = "${node_name}_regen_tests"
-
-                                        for(f in graphml_list){
-                                            def file_path = f.name
-                                            def file_name = utils.trimExtension(file_path)
-                                            def test_name = file_name
-
-                                            print("Running re_gen Test: ${test_name}")
-
-                                            dir(test_name){
-                                                def r_start = new Date()
-                                                def regen_success = run_regen_xsl("${re_path}/re_gen", 'generate_project.xsl', "../${file_path}")
-                                                def r_end = new Date()
-
-                                                def build_success = false
-                                                if(regen_success){
-                                                    dir("build"){
-                                                        build_success = utils.buildProject("Ninja", "")
-                                                    }
-                                                }
-                                                def c_end = new Date()
-
-                                                def regen_time = TimeCategory.minus(r_end, r_start).toMilliseconds() / 1000.0
-                                                def compile_time = TimeCategory.minus(c_end, r_end).toMilliseconds() / 1000.0
-
-                                                def t_time = regen_time + compile_time
-                                                def t_fail_count = (regen_success ? 0 : 1) + (build_success ? 0 : 1)
-                                                def suite_output = get_test_suite_open("Regen_${test_name}", 2, t_fail_count, t_time)
-                                                suite_output += get_test_case("xsl_generation", regen_success, regen_time)
-                                                suite_output += get_test_case("compilation", build_success, compile_time)
-                                                suite_output += get_test_suite_close()
-                                                
-                                                //Update our top level counters
-                                                test_suites += suite_output;
-                                                test_count += 2
-                                                fail_count += t_fail_count
-                                                test_time += t_time
-                                                //Clean up the directory after
-                                                deleteDir()
-                                            }
+                                        if(utils.runScript("python run_tests.py ${test_suite_xml}") != 0){
+                                            error("Running run_tests.py Failed!")
                                         }
-                                        junit_output = get_xml_preamble()
-                                        junit_output += get_test_suites_open("Regen_Tests", test_count, fail_count, test_time)
-                                        junit_output += test_suites
-                                        junit_output += get_test_suites_close()
                                         
-                                        writeFile file: "${test_suite_name}.xml", text: junit_output
-                                        junit  "${test_suite_name}.xml"
+                                        junit test_suite_xml
+                                        dir("build"){
+                                            deleteDir()
+                                        }
                                     }
                                 }
                             }
@@ -268,51 +219,4 @@ pipeline{
             }
         }
     }
-}
-
-
-
-
-def run_regen_xsl(regen_path, transform, file_path, args = ""){
-    def utils = new cditma.Utils(this)
-    return utils.runScript("java -jar ${regen_path}/saxon.jar -xsl:${regen_path}/${transform} -s:${file_path} ${args}") == 0
-}
-
-def get_test_case(test_name, test_pass, test_time, fail_string = ""){
-    def status = 'run'
-    def testcase_args = "name=\"${test_name}\" status=\"${status}\" time=\"${test_time}\""
-    def tab = "\t\t"
-    def output = ""
-    if(test_pass){
-        output += "${tab}<testcase ${testcase_args} />\n"
-    }else{
-        output += "${tab}<testcase ${testcase_args}>\n"
-        output += "${tab}\t<failure message=\"${fail_string}\" />\n"
-        output += "${tab}</testcase>\n"
-    }
-    return output;
-}
-
-def get_test_suite_open(suite_name, test_count, failure_count, test_time){
-    def errors = 0
-    def disabled = 0
-    return "\t<testsuite name=\"${suite_name}\" tests=\"${test_count}\" failures=\"${failure_count}\" errors=\"${errors}\" disabled=\"${disabled}\" time=\"${test_time}\">\n"
-}
-
-def get_test_suite_close(){
-    return "\t</testsuite>\n"
-}
-
-def get_test_suites_open(suite_name, test_count, failure_count, test_time){
-    def errors = 0
-    def disabled = 0
-    return "<testsuites name=\"${suite_name}\" tests=\"${test_count}\" failures=\"${failure_count}\" errors=\"${errors}\" disabled=\"${disabled}\" time=\"${test_time}\">\n"
-}
-
-def get_xml_preamble(){
-    return '<?xml version="1.0" encoding="UTF-8"?>\n'
-}
-
-def get_test_suites_close(){
-    return "</testsuites>\n"
 }
---
+++ test/re_gen/run_tests.py
@@ -0,0 +1,136 @@
+import glob, time, subprocess, os, sys
+
+cd_stack = []
+def push_cd(new_dir):
+    global cd_stack
+    cd_stack = [os.getcwd()] + cd_stack
+    os.chdir(new_dir)
+
+def pop_cd():
+    global cd_stack
+    new_dir = cd_stack.pop(0)
+    os.chdir(new_dir)
+
+def make_dir(dir):
+    try:
+        os.makedirs(dir)
+    except:
+        None
+
+def run_command(args):
+    start_time = time.time()
+    success = True
+    output = ""
+    try:
+        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        success = False
+        output = e.output
+    return [success, output, time.time() - start_time]
+
+
+def run_regen_generation(model_path):
+    re_gen_path = os.environ.get('RE_PATH') + '/re_gen/'
+    args = ['java', '-jar', re_gen_path + 'saxon.jar', '-xsl:' + re_gen_path + 'generate_project.xsl', '-s:' + model_path]
+    return run_command(args)
+
+def run_cmake_generation():
+    args = ['cmake', '-G', 'Ninja', '..']
+    return run_command(args)
+
+def run_cmake_build():
+    args = ['cmake', '--build', '.']
+    return run_command(args)
+
+
+def get_model_name(model_path):
+    base = os.path.basename(model_path)
+    return os.path.splitext(base)[0]
+
+
+def get_test_case(test_name, test_pass, test_time, fail_string = ""):
+    args = 'name="' + test_name + '" status="run" time="' + str(test_time) + '"'
+    tab = '\t\t'
+    
+    output = ""
+    if(test_pass):
+        output += tab + '<testcase ' + args + '/>\n'
+    else:
+        output += tab + '<testcase ' + args + '>\n'
+        output += tab + '\t<failure message="' + fail_string + '" />\n'
+        output += tab + '</testcase>\n'
+    return output
+
+def get_test_suite_open(suite_name, test_count, failure_count, test_time):
+    return '\t<testsuite name="' + suite_name + '" tests="' + str(test_count) + '" failures="' + str(failure_count) + '" errors="0" disabled="0" time="' + str(test_time) + '">\n'
+
+def get_test_suite_close():
+    return "\t</testsuite>\n"
+
+def get_test_suites_open(suite_name, test_count, failure_count, test_time):
+    return '<testsuites name="' + suite_name + '" tests="' + str(test_count) + '" failures="' + str(failure_count) + '" errors="0" disabled="0" time="' + str(test_time) + '">\n'
+
+def get_test_suites_close():
+    return "</testsuites>\n"
+
+def get_xml_preamble():
+    return '<?xml version="1.0" encoding="UTF-8"?>\n'
+
+
+all_suite_output = ""
+total_tests = 0
+total_fails = 0
+total_time = 0
+
+for model_path in glob.glob('models/**.graphml'):
+    model_name = get_model_name(model_path)
+    print("Running Test: " + model_name)
+
+    model_dir = "build/" + model_name
+    make_dir(model_dir)
+    push_cd(model_dir)
+
+    gen_result = run_regen_generation("../../" + model_path)
+
+    make_dir('build')
+    push_cd('build')
+
+    cmake_result = run_cmake_generation()
+    build_result = run_cmake_build()
+
+    # Get back out of the directory
+    pop_cd()
+    pop_cd()
+
+    fail_count = int(not gen_result[0]) + int(not cmake_result[0]) + int(not build_result[0])
+    test_time = gen_result[2] + cmake_result[2] + build_result[2]
+
+    # Generate the suite output
+    suite_output = get_test_suite_open('regen_' + model_name, 3, fail_count, total_time)
+    suite_output += get_test_case("xsl_generation", gen_result[0], gen_result[2], gen_result[1])
+    suite_output += get_test_case("cmake_generation", cmake_result[0], cmake_result[2], cmake_result[1])
+    suite_output += get_test_case("compilation", build_result[0], build_result[2], build_result[1])
+    suite_output += get_test_suite_close()
+
+    # Update our top level counters
+    all_suite_output += suite_output
+    total_tests += 3
+    total_fails += fail_count
+    total_time += test_time
+
+# Write the Junit into a string
+junit_output = get_xml_preamble()
+junit_output += get_test_suites_open("regen_tests", total_tests, total_fails, total_time)
+junit_output += all_suite_output
+junit_output += get_test_suites_close()
+
+
+output_file = 'output.xml'
+if(len(sys.argv) == 2):
+    # Get the output name
+    output_file = sys.argv[1]
+
+print("Writing JUnit: " + output_file)
+f = open(output_file, "w")
+f.write(junit_output)
+f.close()
