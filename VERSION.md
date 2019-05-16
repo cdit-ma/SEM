3096abf86a56524e5799db0bb3c91d74f27cd3f3
Mitchell Conrad <mitchell.w.conrad@gmail.com>
2019-05-17 09:28:01 +0930

    Release v3.3.7 (#128)
    * Create release branch.
    
    * Add error handling to rebuild docker images jenkins job
    
    * Update logan ref.
    
    * Update cmake_modules ref.
    
    * Update logan ref with cmake fix.
    
    * Update re_common ref.
    
    * Fix docker build context.
    
    * Fix naming mismatch in rebuild_docker_images jenkins job.
    
    * Fix typo/omission in rebuild docker images script.
    
    * Add base image arg to logan rebuild, remove pull requirement for rebuild.
    
    * Update logan ref.

--- VERSION.md
+++ VERSION.md
@@ -1,5 +1,10 @@
 # Runtime Environment v3.3.7
 
+## Logan
+* Rolled logan aggregation_server and aggregation_broker into mainline release
+
+## Code Generation
+* Fixed multiple code generation bugs
 ## Resolved Jira Tickets:
 * MED-580
 * RE-461
--- jenkins/rebuild_docker_images.groovy
+++ jenkins/rebuild_docker_images.groovy
@@ -8,7 +8,7 @@ def deploy_map = [:]
 pipeline{
     agent none
     parameters{
-        string(name: 're_base_image_name', defaultValue: '', description: 'Docker base image name')
+        string(name: 're_base_image_name', defaultValue: '', description: 'Docker base image name (full_deps if other dependencies are not needed)')
         booleanParam(name: 'rebuild_re_image', defaultValue: true, description: 'Rebuild re image')
         booleanParam(name: 'rebuild_logan_image', defaultValue: true, description: 'Rebuild logan image')
     }
@@ -17,26 +17,38 @@ pipeline{
             steps{
                 script{
                     node("docker_runtime") {
+                        def base_image_arg = ""
+                        def base_image_name = "full_deps"
+                        if(params.re_base_image_name) {
+                            base_image_arg = "--build-arg base_image=${DOCKER_REGISTRY_ENDPOINT}/${params.re_base_image_name}"
+                            base_image_name = "${params.re_base_image_name}"
+                        }
+                        def base_image_registry_name = "${DOCKER_REGISTRY_ENDPOINT}/${base_image_name}"
+                        
                         if(params.rebuild_re_image){
-                            def base_image_arg = ""
-                            if(params.re_base_image_name) {
-                                base_image_arg = "--build-arg base_image=${params.re_base_image_name}"
-                            }
                             dir("${HOME}/re") {
-                                // Rebuild docker image
-                                utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/${params.re_base_image_name}")
-                                utils.runScript("docker build -t re_full -f docker/re_full/Dockerfile ${base_image_arg} .")
-                                utils.runScript("docker tag re_full ${DOCKER_REGISTRY_ENDPOINT}/re_full")
-                                utils.runScript("docker push ${DOCKER_REGISTRY_ENDPOINT}/re_full")
+                                if(utils.runScript("docker build -t re_full -f docker/re_full/Dockerfile ${base_image_arg} .") != 0) {
+                                    error("Failed to build re_full image")
+                                }
+                                if(utils.runScript("docker tag re_full ${DOCKER_REGISTRY_ENDPOINT}/re_full") != 0) {
+                                    error("Failed to tag re_full image")
+                                }
+                                if(utils.runScript("docker push ${DOCKER_REGISTRY_ENDPOINT}/re_full") != 0) {
+                                    error("Failed to push re_full image after build")
+                                }
                             }
                         }
                         if(params.rebuild_logan_image) {
-                            dir("${HOME}/re/logan") {
-                                // Rebuild docker image
-                                utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/logan")
-                                utils.runScript("docker build -t logan -f docker/aggregation_server/Dockerfile .")
-                                utils.runScript("docker tag logan ${DOCKER_REGISTRY_ENDPOINT}/logan")
-                                utils.runScript("docker push ${DOCKER_REGISTRY_ENDPOINT}/logan")
+                            dir("${HOME}/re") {
+                                if(utils.runScript("docker build -t logan -f logan/docker/aggregation_server/Dockerfile ${base_image_arg} .") != 0) {
+                                    error("Failed to build logan image")
+                                }
+                                if(utils.runScript("docker tag logan ${DOCKER_REGISTRY_ENDPOINT}/logan") != 0) {
+                                    error("Failed to tag logan image")
+                                }
+                                if(utils.runScript("docker push ${DOCKER_REGISTRY_ENDPOINT}/logan") != 0) {
+                                    error("Failed to push logan image after build")
+                                }
                             }
                         }
                     }
@@ -54,8 +66,13 @@ pipeline{
 
                         deploy_map[node_name] = {
                             node(node_name){
-                                utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/re_full")
-                                utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/logan")
+                                if(utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/re_full") != 0){
+                                    error("Failed to pull re_full docker image on ${node_name}")
+                                }
+
+                                if(utils.runScript("docker pull ${DOCKER_REGISTRY_ENDPOINT}/logan") != 0) {
+                                    error("Failed to pull logan docker image on ${node_name}")
+                                }
                             }
                         }
                     }
