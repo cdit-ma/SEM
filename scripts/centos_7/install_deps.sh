00ccbbce61e8819828d695a95b3f77d6a766db4a
JacksonM8 <jackson.m840@gmail.com>
2019-12-04 17:18:54 +1030

    Adding DockerClient external library SEM-74 (#165)
    * Adding DockerClient external library SEM-75
    
    * Updating docker build dependencies to include libcurl dev headers SEM-75
    
    * Accommodate older CMake versions & not build RapidJSON tests SEM-75
    
    * Updated variable declaration in submodule cmake SEM-75
    
    * Updated include directories in cmake SEM-75
    
    * Update submodule ref.

--- scripts/centos_7/install_deps.sh
+++ scripts/centos_7/install_deps.sh
@@ -6,6 +6,7 @@ sudo yum install epel-release git wget java-1.8.0-openjdk chrony && \
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
     sudo yum install docker-ce docker-ce-cli containerd.io && \
     sudo yum install ninja-build cmake3 ccache python-devel && \
+    sudo yum install libcurl-devel && \
     sudo ln -s /usr/bin/cmake3 /usr/bin/cmake && \
     . /opt/rh/devtoolset-7/enable
 
