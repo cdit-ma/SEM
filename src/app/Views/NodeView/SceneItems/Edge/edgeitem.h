6b99722d3827c161dec308d2d10d63f8c9645573
cathlynaston <cm.miller@y7mail.com>
2020-03-19 17:00:14 +1030

    Created a getRect helper function that takes in a center point and size (#243)

--- src/app/Views/NodeView/SceneItems/Edge/edgeitem.h
+++ src/app/Views/NodeView/SceneItems/Edge/edgeitem.h
@@ -7,27 +7,27 @@
 class NodeItem;
 class EdgeItem : public EntityItem
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     EdgeItem(EdgeViewItem* edgeViewItem, NodeItem* parent, NodeItem* source, NodeItem* destination);
     ~EdgeItem() final;
-
+    
     void setPos(const QPointF &pos) final;
     QPointF getPos() const final;
-
+    
     QRectF boundingRect() const final;
     QRectF edgeRect() const;
     QRectF currentRect() const final;
     
     QPointF getSceneEdgeTermination(EDGE_DIRECTION direction, EDGE_KIND kind) const final;
-
+    
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) final;
     QRectF getElementRect(EntityRect rect) const final;
     QPainterPath getElementPath(EntityRect rect) const final;
-
+    
     bool setMoveFinished() final;
-
+    
     bool isCentered() const;
     void resetCenter();
     void setCentered(bool centered);
@@ -52,33 +52,35 @@ private:
     void sourceIconHover(bool handle, const QPointF& pos);
     void targetIconHover(bool handle, const QPointF& pos);
     void moveHover(bool handle, const QPointF& pos);
-
+    
     //Rectangle helpers
+    QRectF getRect(const QPointF& center, const QSizeF& size) const;
+    
     QRectF srcIconRect() const;
     QRectF centerIconRect() const;
     QRectF dstIconRect() const;
-
+    
     QRectF srcIconCircle() const;
     QRectF dstIconCircle() const;
-
-    QRectF centerCircleRect() const;
-    QRectF translatedCenterCircleRect(QPointF center = QPointF()) const;
-
-    QPointF getCenterCircleTermination(bool left, QPointF center = QPointF()) const;
-
+    
+    QSizeF centerCircleRectSize() const;
+    QRectF translatedCenterCircleRect(const QPointF& center = QPointF()) const;
+    
+    QPointF getCenterCircleTermination(bool left, const QPointF& center = QPointF()) const;
+    
     void updateSrcCurve(QPointF srcP, QPointF ctrP, bool srcP_Left, bool ctrP_Left);
     void updateDstCurve(QPointF dstP, QPointF ctrP, bool dstP_Left, bool ctrP_Left);
-
-    QPainterPath calculateArrowHead(QPointF startPoint, bool pointLeft) const;
+    
+    QPainterPath calculateArrowHead(const QPointF& endPoint, bool pointLeft) const;
     QPainterPath calculateBezierCurve(QPointF P1, QPointF P2, bool P1_Left, bool P2_Left) const;
-
+    
     NodeItem* getFirstVisibleParent(NodeItem* item);
-
+    
     NodeItem* src = nullptr;
     NodeItem* dst = nullptr;
     NodeItem* vSrc = nullptr;
     NodeItem* vDst = nullptr;
-
+    
     QPainterPath srcCurve;
     QPainterPath dstCurve;
     QPainterPath srcArrow;
