4ed9f56fc35925133792bf3515f6fda58308a518
cathlynaston <cm.miller@y7mail.com>
2020-03-31 11:31:01 +1030

    Finished going through the theme class

--- src/app/theme.h
+++ src/app/theme.h
@@ -19,31 +19,23 @@
 #include "Controllers/SettingsController/settingscontroller.h"
 #include "Controllers/NotificationManager/notificationenumerations.h"
 
-
-#define THEME_STYLE_QMENU "THEME_STYLE_QMENU"
-#define THEME_STYLE_QPUSHBUTTON "THEME_STYLE_QPUSHBUTTON"
-#define THEME_STYLE_QPUSHBUTTON_JENKINS "THEME_STYLE_QPUSHBUTTON_JENKINS"
-#define THEME_STYLE_GROUPBOX "THEME_STYLE_GROUPBOX"
-#define THEME_STYLE_HIDDEN_TOOLBAR "HIDDEN_TOOLBAR"
-
 typedef QPair<QString, QString> IconPair;
 Q_DECLARE_METATYPE(IconPair); 
 
 enum class ThemePreset{XMAS_THEME, LIGHT_THEME, DARK_THEME, SOLARIZED_DARK_THEME, SOLARIZED_LIGHT_THEME};
 enum class ColorRole{NONE, NORMAL, DISABLED, SELECTED};
 
-
-
 class Theme: public QObject
 {
     Q_OBJECT
+
 protected:
     Theme();
-    ~Theme();
-public:
+    ~Theme() final;
 
-    QColor white();
-    QColor black();
+public:
+    static QColor white();
+    static QColor black();
 
     QColor getBackgroundColor();
     QString getBackgroundColorHex();
@@ -62,7 +54,6 @@ public:
 
     QMovie* getGif(const QString& path, const QString& name);
 
-
     QColor getActiveWidgetBorderColor();
     QString getActiveWidgetBorderColorHex();
 
@@ -75,14 +66,10 @@ public:
     QColor getAspectBackgroundColor(VIEW_ASPECT aspect);
 
     QColor getMainImageColor(const QString& prefix, const QString& alias);
-    QColor getMainImageColor(IconPair path);
 
-    QIcon getIcon(IconPair icon);
+    QIcon getIcon(const IconPair& icon);
     QIcon getIcon(const QString& prefix, const QString& alias);
 
-private:
-    QPixmap _getPixmap(const QString& resourceName, QSize size = QSize(), QColor tintColor = QColor());
-public:
     QPixmap getImage(const QString& prefix, const QString& alias, QSize size = QSize(), QColor tintColor = QColor());
 
     QString getPixmapResourceName(QString resource_name, QSize size, QColor tintColor);
@@ -90,29 +77,28 @@ public:
     QString getAltTextColorHex();
     void setAltTextColor(QColor color);
 
-    void setBackgroundColor(QColor color);
-    void setDisabledBackgroundColor(QColor color);
-    void setAltBackgroundColor(QColor color);
-    void setHighlightColor(QColor color);
-    void setActiveWidgetBorderColor(QColor color);
-    void setTextColor(ColorRole role, QColor color);
-    void setMenuIconColor(ColorRole role, QColor color);
-    void setAspectBackgroundColor(VIEW_ASPECT aspect, QColor color);
+    void setBackgroundColor(const QColor& color);
+    void setDisabledBackgroundColor(const QColor& color);
+    void setAltBackgroundColor(const QColor& color);
+    void setHighlightColor(const QColor& color);
+    void setActiveWidgetBorderColor(const QColor& color);
+    void setTextColor(ColorRole role, const QColor& color);
+    void setMenuIconColor(ColorRole role, const QColor& color);
+    void setAspectBackgroundColor(VIEW_ASPECT aspect, const QColor& color);
     void setIconToggledImage(const QString& prefix, const QString& alias, const QString& toggledOnPrefix, const QString& toggledOnAlias, const QString& toggledOffPrefix, const QString& toggleOffAlias, bool ignore_toggle_coloring = true);
     void setInactiveEdgeOpacity(qreal opacity);
 
     void setWindowIcon(const QString& window_title, const QString& visible_icon_prefix, const QString& visible_icon_alias);
 
     void setIconAlias(const QString& prefix, const QString& alias, const QString& icon_prefix, const QString& icon_alias);
-    void setDefaultImageTintColor(const QString& prefix, const QString& alias, QColor color);
 
-    void setSeverityColor(Notification::Severity severity, QColor color);
+    void setSeverityColor(Notification::Severity severity, const QColor& color);
     QColor getSeverityColor(Notification::Severity);
 
     void applyTheme();
     bool isValid() const;
 
-    bool gotImage(IconPair icon);
+    bool gotImage(const IconPair& icon);
     bool gotImage(const QString&, const QString&);
     bool gotImage(const QString&);
 
@@ -124,8 +110,8 @@ public:
     QSize getIconSize();
     QSize getLargeIconSize();
 
-    QString getCornerRadius();
-    QString getSharpCornerRadius();
+    static QString getCornerRadius();
+    static QString getSharpCornerRadius();
 
     qreal getInactiveEdgeOpacity();
 
@@ -135,12 +121,12 @@ public:
     QString getScrollBarStyleSheet();
     QString getDialogStyleSheet();
     QString getSplitterStyleSheet();
-    QString getWidgetStyleSheet(QString widgetName="QWidget");
+    QString getWidgetStyleSheet(const QString& widgetName = "QWidget");
     QString getTabbedWidgetStyleSheet();
-    QString getNodeViewStyleSheet(bool isActive=false);
+    QString getNodeViewStyleSheet(bool isActive = false);
     QString getDockWidgetStyleSheet();
-    QString getViewDockWidgetStyleSheet(bool isActive=false);
-    QString getDockTitleBarStyleSheet(bool isActive=false, QString widgetName="DockTitleBar");
+    QString getViewDockWidgetStyleSheet(bool isActive = false);
+    QString getDockTitleBarStyleSheet(bool isActive = false, const QString& widgetName="DockTitleBar");
     QString getToolDockWidgetTitleBarStyleSheet();
     QString getMenuBarStyleSheet();
     QString getMenuStyleSheet(int icon_size = 32);
@@ -149,69 +135,76 @@ public:
     QString getAbstractItemViewStyleSheet();
     QString getGroupBoxStyleSheet();
     QString getPushButtonStyleSheet();
-    QString getLineEditStyleSheet(QString widget_name="QLineEdit");
+    QString getLineEditStyleSheet(const QString& widget_name = "QLineEdit");
     QString getProgressBarStyleSheet();
     QString getLabelStyleSheet();
     QString getSliderStyleSheet();
     QString getSpinBoxStyleSheet(const QString& spinbox_class = "QSpinBox");
-
-    QString getTitleLabelStyleSheet();
-    QString getAspectButtonStyleSheet(VIEW_ASPECT aspect);
     QString getToolTipStyleSheet();
 
     QFont getFont() const;
     QFont getSmallFont() const;
     QFont getLargeFont() const;
 
+    static QString QColorToHex(const QColor& color);
+    static Theme* theme();
+    static void roundQSize(QSize& size);
+    static IconPair getIconPair(const QString& prefix, const QString& alias);
+
+    static void UpdateActionIcon(QAction* action, Theme* theme = nullptr);
+    static void StoreActionIcon(QAction* action, const QString& alias, const QString& name);
+    static void StoreActionIcon(QAction* action, const IconPair& icon);
+
 signals:
     void theme_Changed();
     void changeSetting(SETTINGS setting, QVariant value);
+
 public slots:
-    void settingChanged(SETTINGS setting, QVariant value);
+    void settingChanged(SETTINGS setting, const QVariant& value);
+
 private:
+    QPixmap _getPixmap(const QString& resourceName, QSize size = QSize(), QColor tintColor = QColor());
+
     void preloadImages();
 
-struct ImageLoad{
-    QString resource_name;
-    QPair<QString, QString> image_path;
-    QImage image;
-    QColor color;
-};
+    struct ImageLoad {
+        QString resource_name;
+        QPair<QString, QString> image_path;
+        QImage image;
+        QColor color;
+    };
+
     static ImageLoad LoadImage(const QString& resource_name);
     static QColor CalculateImageColor(const QImage& image);
 
-    QString getResourceName(QString prefix, QString alias) const;
-    QString getResourceName(IconPair icon) const;
+    QString getResourceName(const QString& prefix, const QString& alias) const;
+    QString getResourceName(const IconPair& icon) const;
+
     void resetTheme(ThemePreset themePreset);
     void resetAspectTheme(bool colorBlind);
     void setupToggledIcons();
     void setupAliasIcons();
     void updateValid();
     
-    void setFont(QFont size);
+    void setFont(const QFont& font);
 
     QImage getImage(const QString& resource_name);
 
     static IconPair SplitImagePath(const QString& path);
 
-    bool tintIcon(IconPair pair);
-    bool tintIcon(const QString& prefix, const QString& alias);
+    // TODO: Maybe rename these to canTintIcon?
     bool tintIcon(QSize size);
     bool tintIcon(const QString& resource_name);
 
     QSet<QString> image_names;
 
     QHash<QString, QMovie*> gifLookup;
-
     QHash<QString, QImage> imageLookup;
     QHash<QString, QPixmap> pixmapLookup;
     QHash<QString, QIcon> iconLookup;
-
-
     QHash<QString, QSize> pixmapSizeLookup;
-    QHash<QString, QColor> pixmapTintLookup;
 
-    struct IconToggle{
+    struct IconToggle {
         IconPair on;
         IconPair off;
         bool got_toggle = false;
@@ -225,19 +218,17 @@ struct ImageLoad{
         ColorRole off_disabled = ColorRole::DISABLED;
     };
 
-    QHash<QString, IconToggle > iconToggledLookup;
+    QHash<QString, IconToggle> iconToggledLookup;
     QHash<QString, QColor> pixmapMainColorLookup;
 
     QHash<VIEW_ASPECT, QColor> aspectColor;
-
     QHash<Notification::Severity, QColor> severityColor;
 
     QHash<ColorRole, QColor> textColor;
     QHash<ColorRole, QColor> menuIconColor;
 
     QHash<QString, QString> icon_alias_lookup;
-
-    QHash<QString, QSet<QString> > icon_prefix_lookup;
+    QHash<QString, QSet<QString>> icon_prefix_lookup;
 
     QColor highlightColor;
     QColor backgroundColor;
@@ -247,34 +238,22 @@ struct ImageLoad{
     QColor altTextColor;
 
     QSize icon_size = QSize(16,16);
-
     qreal inactive_opacity_ = 0.50;
-
     QFont font;
 
     QReadWriteLock lock_;
-    QReadWriteLock pixmap_lock_;
 
-    ThemePreset current_theme = ThemePreset::DARK_THEME;
     bool themeChanged = false;
     bool valid = false;
-    QFuture<void> entity_icons_load_future;
-public:
-    static QString QColorToHex(const QColor& color);
-    static Theme* theme();
-    static void roundQSize(QSize& size);
-    static IconPair getIconPair(const QString& prefix, const QString& alias);
 
-    static void UpdateActionIcon(QAction* action, Theme* theme = 0);
-    static void StoreActionIcon(QAction* action, QString alias, QString name);
-    static void StoreActionIcon(QAction* action, IconPair icon);
+    QFuture<void> entity_icons_load_future;
 };
 
 
-class CustomMenuStyle : public QProxyStyle{
+class CustomMenuStyle : public QProxyStyle {
     public:
-        CustomMenuStyle(int icon_size = 32);
-        int pixelMetric(PixelMetric metric, const QStyleOption* option = 0, const QWidget* widget = 0) const;
+        explicit CustomMenuStyle(int icon_size = 32);
+        int pixelMetric(PixelMetric metric, const QStyleOption* option, const QWidget* widget) const override;
     private:
         int icon_size = 32;
 };
@@ -283,6 +262,5 @@ inline uint qHash(ColorRole key, uint seed)
 {
     return ::qHash(static_cast<uint>(key), seed);
 }
-
 
 #endif // THEME_H
