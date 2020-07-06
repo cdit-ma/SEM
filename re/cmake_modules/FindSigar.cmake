b5e5e04c27a1de91dbc53907acb3129cc028c2f1
mitchell-conrad <mitchell.w.conrad@gmail.com>
2020-07-06 12:02:36 +0930

    Use consistent capitalisation for Sigar CMake find and usage.

--- re/cmake_modules/FindSigar.cmake
+++ re/cmake_modules/FindSigar.cmake
@@ -1,26 +1,26 @@
-find_path(SIGAR_ROOT_DIR
+find_path(Sigar_ROOT_DIR
     NAMES include/sigar.h
 )
 
-find_library(SIGAR_LIBRARIES
+find_library(Sigar_LIBRARIES
     NAMES sigar libsigar
-    HINTS ${SIGAR_ROOT_DIR}/build-src
-    HINTS ${SIGAR_ROOT_DIR}/lib
+    HINTS ${Sigar_ROOT_DIR}/build-src
+    HINTS ${Sigar_ROOT_DIR}/lib
 )
 
-find_path(SIGAR_INCLUDE_DIRS
+find_path(Sigar_INCLUDE_DIRS
     NAMES sigar.h
-    HINTS ${SIGAR_ROOT_DIR}/include
+    HINTS ${Sigar_ROOT_DIR}/include
 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Sigar DEFAULT_MSG
-    SIGAR_LIBRARIES
-    SIGAR_INCLUDE_DIRS
+    Sigar_LIBRARIES
+    Sigar_INCLUDE_DIRS
 )
 
 mark_as_advanced(
-    SIGAR_ROOT_DIR
-    SIGAR_LIBRARIES
-    SIGAR_INCLUDE_DIRS
+    Sigar_ROOT_DIR
+    Sigar_LIBRARIES
+    Sigar_INCLUDE_DIRS
 )
