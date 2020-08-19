1baadcf9ee0b33ce55aa9878c1761786f4c5f834
Mitchell Conrad <mitchell.w.conrad@gmail.com>
2020-08-19 12:33:35 +0930

    Cache depencies and add windows sccache support (#9)
    * Added a new exception for handling malformed proto messages in the AggregationProxy. Now catching experiments that have an empty experiment name.
    
    * Added a temporary fix to disallowing the user to run a Jenkins job with an empty experiment name
    
    * Removed dead function declaration
    
    * Stopped jobs from building when any of the required parameters is empty.
    
    * Changed to showing min/max utilisation of the y-axis instead of min/max of the displayed series
    
    * Moved min/max utilisation to the left
    
    * Missing Marker charts SEM-540 (#264)
    
    * MarkerSetData is now being constructed when Marker events are requested. Once they are constructed and the events are added to them, the charts for the contained MarkerEventSeries are constructed and shown.
    
    * Added documentation to functions
    
    * Fixed rendering of Marker event series.
    
    * Apply suggestions from code review and addressed review comments
    
    * Release v3.4.3 (#265)
    
    * Updated version number and changelog for v3.4.3
    
    * Updated re submodule to match v3.4.3
    
    * Re-add chart filters SEM-572 (#266)
    
    * MarkerSetData is now being constructed when Marker events are requested. Once they are constructed and the events are added to them, the charts for the contained MarkerEventSeries are constructed and shown.
    
    * Added documentation to functions
    
    * Fixed rendering of Marker event series.
    
    * Overloaded getter functions in the data classes to include filters for series
    
    * Fixed filtering of displayed charts through user selection.
    
    * Fixed a couple of bugs with updating ComponentInstanceData and its children.
    
    * Fixed rendering of Marker event series.
    
    * Overloaded getter functions in the data classes to include filters for series
    
    * Fixed filtering of displayed charts through user selection.
    
    * Fixed a couple of bugs with updating ComponentInstanceData and its children.
    
    * Fixed bug changing the displayed time format. The timeline axis was not updating its displayed values completely until the displayed range is changed by the user.
    
    * Apply suggestions from code review
    
    * Exception printouts (#267)
    
    * Added extra catches for the request functions in ExperimentDataManager to show more into for the custom exceptions.
    
    * Added minor const changes.
    
    * Update src/app/Controllers/AggregationProxy/aggregationproxy.h
    
    Co-authored-by: JacksonM8 <jackson.m840@gmail.com>
    
    * Add extern deps dir capability to top level cmake.
    
    * Update build_sem to handle cached dependency source.
    
    * Remove warning.
    
    * Add sccache condition to windows top level cmake.
    
    * Remove Zi compiler flag if MSVC.
    
    * Remove Zi compiler flag if MSVC after adding extern projects.
    
    * Stop gtest from using sccache.
    
    * Take 2.
    
    * Revert part of prev commit.
    
    * Skip pqxx tests.
    
    * Apply suggestions from code review
    
    Co-authored-by: JacksonM8 <jackson.m840@gmail.com>
    
    * Test that deps cache dir env var exists. Test that we're using sccache on windows before building gtest.
    
    * Take two at cmake if condition.
    
    * Fix unset deps cache dir case.
    
    Co-authored-by: cathlynaston <cm.miller@y7mail.com>
    Co-authored-by: JacksonM8 <jackson.m840@gmail.com>

--- src/jenkins/build_sem.groovy
+++ src/jenkins/build_sem.groovy
@@ -71,7 +71,11 @@ pipeline{
                                 unstash "source_code"
                                 dir("build"){
                                     deleteDir()
-                                    if(!utils.buildProject("Ninja", "-DBUILD_TEST=ON")){
+                                    def generate_args = "-DBUILD_TEST=ON"
+                                    if(env.SEM_DEPS_CACHE_DIR){
+                                        generate_args = generate_args + " -DSEM_DEPS_BASE_DIR=${env.SEM_DEPS_CACHE_DIR}"
+                                    }
+                                    if(!utils.buildProject("Ninja", generate_args)){
                                         error("CMake failed on Builder Node: ${node_name}")
                                     }
                                 }
