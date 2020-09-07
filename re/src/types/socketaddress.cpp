37271a48a786cfaeeb1dae3dcad081072adb3320
Mitchell Conrad <mitchell.w.conrad@gmail.com>
2020-09-07 14:47:30 +0930

    Add constructors for Ipv4 and SocketAddress. (#16)
    * Add constructors for Ipv4 and SocketAddress. Deprecate old ::from_* static factory methods.
    
    * Remove evil default initialisation from Ipv4 octet array.
    
    * Move most of heavy implementation things to ipv4 and socketaddress cpp files.
    
    * Remove nodiscard attr on ipv4 assignment operators. Add assignment and copy/move constructors to socket address.
    
    * Remove (now unnecessary) static_cast<void> due to removed [[nodiscard]] attr.
    
    * Fix missing <ostream> include.

---
+++ re/src/types/socketaddress.cpp
@@ -0,0 +1,40 @@
+//
+// Created by mitchell on 7/9/20.
+//
+#include "socketaddress.hpp"
+#include <regex>
+#include <variant>
+namespace re::types {
+
+namespace detail {
+auto parse_port_from_string(const std::string& port) -> uint16_t
+{
+    long port_number = std::stoi(port);
+    if(port_number > 65535 || port_number < 0) {
+        throw std::invalid_argument("port given is out of range");
+    }
+    return static_cast<uint16_t>(port_number);
+}
+} // namespace detail
+
+auto SocketAddress::parse_socket_address(const std::string& addr_string)
+    -> std::pair<Ipv4, uint16_t>
+{
+    // Break out early if we're too long to avoid pathological regex case.
+    if(addr_string.length() > 21) {
+        throw std::invalid_argument("Invalid socket address string format; too long.");
+    }
+
+    const std::regex addr_check{R"--(^(.*):(\d{1,5})$)--"};
+    std::smatch matches;
+    if(!std::regex_match(addr_string.begin(), addr_string.end(), matches, addr_check)) {
+        throw std::invalid_argument("Invalid socket address string format; cannot parse.");
+    }
+    try {
+        return {Ipv4(matches[1].str()), detail::parse_port_from_string(matches[2].str())};
+    } catch(const std::exception& ex) {
+        throw std::invalid_argument(std::string("Invalid socket address string format; ")
+                                    + ex.what());
+    }
+}
+} // namespace re::types
--- re/src/types/test/socketaddress.cpp
+++ re/src/types/test/socketaddress.cpp
@@ -4,10 +4,10 @@
 namespace re::types::test {
 TEST(re_types_socketaddress, equality_operator)
 {
-    constexpr auto a = SocketAddress::from_ipv4(Ipv4::from_ints(0, 0, 0, 0), 0);
-    constexpr auto b = SocketAddress::from_ipv4(Ipv4::from_ints(0, 0, 0, 0), 0);
-    constexpr auto c = SocketAddress::from_ipv4(Ipv4::from_ints(0, 0, 0, 0), 10);
-    constexpr auto d = SocketAddress::from_ipv4(Ipv4::from_ints(0, 0, 0, 0), 1);
+    constexpr auto a = SocketAddress({0, 0, 0, 0}, 0);
+    constexpr auto b = SocketAddress(Ipv4(0, 0, 0, 0), 0);
+    constexpr auto c = SocketAddress(Ipv4(0, 0, 0, 0), 10);
+    constexpr auto d = SocketAddress(Ipv4(0, 0, 0, 0), 1);
 
     ASSERT_TRUE(a == b);
     ASSERT_FALSE(a == c);
@@ -16,30 +16,27 @@ TEST(re_types_socketaddress, equality_operator)
 
 TEST(re_types_socketaddress, to_string)
 {
-    constexpr auto addr = SocketAddress::from_ipv4(Ipv4::from_ints(192, 168, 111, 230), 20000);
+    constexpr auto addr = SocketAddress(Ipv4(192, 168, 111, 230), 20000);
 
     ASSERT_EQ(addr.to_string(), "192.168.111.230:20000");
 }
 
 TEST(re_types_socketaddress, from_string)
 {
-    auto addr = SocketAddress::from_string("192.168.1.99:12345");
+    auto addr = SocketAddress("192.168.1.99:12345");
     ASSERT_EQ(addr.to_string(), "192.168.1.99:12345");
 
-    // Static cast to void to dodge [[nodiscard]] warnings....
-    // Seems terrible, couldn't find warning number in any documentation, couldn't use pragma
-    // warning disable.
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230:adsf")),
+    EXPECT_THROW(SocketAddress("192.168.111.230:invalid_port_number_string"),
                  std::invalid_argument);
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230:")),
+    EXPECT_THROW(SocketAddress("192.168.111.230:"),
                  std::invalid_argument);
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230")),
+    EXPECT_THROW(SocketAddress("192.168.111.230"),
                  std::invalid_argument);
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230:1111111111")),
+    EXPECT_THROW(SocketAddress("192.168.111.230:1111111111"),
                  std::invalid_argument);
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230:123adsf")),
+    EXPECT_THROW(SocketAddress("192.168.111.230:123invalid"),
                  std::invalid_argument);
-    EXPECT_THROW(static_cast<void>(SocketAddress::from_string("192.168.111.230:123 4")),
+    EXPECT_THROW(SocketAddress("192.168.111.230:123 4"),
                  std::invalid_argument);
 }
 } // namespace re::types::test
