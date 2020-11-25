2cbb2f1325f5e32e058d24c188116651643ab9c7
mitchell-conrad <mitchell.w.conrad@gmail.com>
2020-11-25 13:53:47 +1030

    Improve signal handling in nodemanagermain.cpp.

--- re/src/nodemanager/nodemanagermain.cpp
+++ re/src/nodemanager/nodemanagermain.cpp
@@ -2,11 +2,17 @@
 #include "nodemanager.h"
 #include "nodemanager_config/nodemanagerconfig.h"
 
+std::optional<sem::node_manager::NodeManager> node_manager;
 std::function<void(void)> interrupt_function;
 
 void signal_handler(int sig)
 {
-    interrupt_function();
+    if(node_manager) {
+        node_manager->shutdown();
+    } else {
+        std::cerr << "Signal handler called before NodeManager initialisation complete"
+                  << std::endl;
+    }
 }
 
 auto main(int argc, char** argv) -> int
@@ -18,20 +24,20 @@ auto main(int argc, char** argv) -> int
         // Parse our command line args, these are consumed at this point.
         auto config = NodeConfig::HandleArguments(argc, argv);
         if(config.has_value()) {
+            // Save our config file immediately in case we had to generate a uuid
+            //  We do this before testing for a vaild epm exe s.t. we always fail with the same uuid
+            NodeConfig::SaveConfigFile(config.value());
+
             // Test that we can find the EPM executable before doing anything else.
             // Fail early my dudes.
             config->find_epm_executable();
 
-            // Save our config file immediately in case we had to generate a uuid
-            NodeConfig::SaveConfigFile(config.value());
-            std::unique_ptr<epm_registry::EpmRegistry> epm_registry = std::make_unique<epm_registry::EpmRegistryImpl>(
-                config.value());
+            std::unique_ptr<epm_registry::EpmRegistry> epm_registry =
+                std::make_unique<epm_registry::EpmRegistryImpl>(config.value());
 
-            sem::node_manager::NodeManager node_manager{config.value(), *epm_registry};
+            node_manager.emplace(config.value(), *epm_registry);
 
-            interrupt_function = [&node_manager]() { node_manager.shutdown(); };
-
-            node_manager.wait();
+            node_manager->wait();
         }
     } catch(const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
