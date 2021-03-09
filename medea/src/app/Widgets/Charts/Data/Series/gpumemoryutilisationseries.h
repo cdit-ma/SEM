1a49488c817b6ed68c86751310dc87b7e09b559c
cathlynaston <cm.miller@y7mail.com>
2021-03-09 12:38:47 +1030

    Implemented GPU compute, memory and temperature Event and EventSeries classes.

---
+++ medea/src/app/Widgets/Charts/Data/Series/gpumemoryutilisationseries.h
@@ -0,0 +1,29 @@
+//
+// Created by Cathlyn Aston on 6/3/21.
+//
+
+#ifndef GPUMEMORYUTILISATIONSERIES_H
+#define GPUMEMORYUTILISATIONSERIES_H
+
+#include <cfloat>
+#include "eventseries.h"
+#include "../Events/gpumemoryutilisationevent.h"
+
+class GPUMemoryUtilisationSeries : MEDEA::EventSeries
+{
+    Q_OBJECT
+
+public:
+    explicit GPUMemoryUtilisationSeries(const QString& ID, QObject* parent = nullptr);
+
+    void addEvent(MEDEA::Event* event) override;
+
+    double getMinUtilisation() const;
+    double getMaxUtilisation() const;
+
+private:
+    double min_utilisation_ = DBL_MAX;
+    double max_utilisation_ = 0.0;
+};
+
+#endif // GPUMEMORYUTILISATIONSERIES_H
